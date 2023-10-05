eeeaa628a06e7b0a39e6be51df8a9667bcefd705
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-05 18:05:05 +0200

    config functions

---
+++ src/config/config.rs
@@ -0,0 +1,72 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use anyhow::{bail, Context, Result};
+use directories::ProjectDirs;
+use log::debug;
+
+use crate::models::config::{Config, UpdateConfig};
+
+fn create_config(path: &Path) -> Result<String> {
+    let new_config = Config::new();
+
+    let toml_string = toml::to_string(&new_config)
+        .with_context(|| format!("Could not create toml config file"))?;
+    debug!("new toml string: {}", &toml_string);
+
+    fs::write(path, &toml_string)?;
+
+    Ok(toml_string)
+}
+
+pub fn get_config_path() -> Result<PathBuf> {
+    let dir_path = ProjectDirs::from("app", "env-ease", "env-ease.toml");
+
+    match dir_path {
+        Some(dirs) => Ok(dirs.config_dir().to_path_buf()),
+        None => bail!("Could not find config directory"),
+    }
+}
+
+pub fn get_config() -> Result<Config> {
+    if let Some(proj_dirs) = ProjectDirs::from("app", "env-ease", "env-ease.toml") {
+        let config_dir = proj_dirs.config_dir();
+        let config_file_exists = Path::new(&config_dir).is_file();
+
+        match config_file_exists {
+            true => {
+                let content = fs::read_to_string(config_dir)?;
+                let data =
+                    toml::from_str::<Config>(&content).context("Could not parse config file")?;
+
+                Ok(data)
+            }
+            false => {
+                let new_config = create_config(config_dir)?;
+                let data = toml::from_str::<Config>(&new_config)?;
+
+                Ok(data)
+            }
+        }
+    } else {
+        bail!("Could not find config directory")
+    }
+}
+
+pub fn update_config(args: UpdateConfig) -> Result<()> {
+    let config_path = get_config_path()?;
+    let mut config = get_config()?;
+
+    let UpdateConfig { token } = args;
+
+    if let Some(new_token) = token {
+        config.token = Some(new_token);
+    }
+
+    let config_string = toml::to_string(&config)?;
+    fs::write(config_path, config_string)?;
+
+    Ok(())
+}
