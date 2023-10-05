efbe821dd422bf936e06c419a459d292aee5583d
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-05 20:55:18 +0200

    project struct

---
+++ src/models/projects.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Project {
+    pub name: String,
+    // date string
+    pub created_at: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+}
