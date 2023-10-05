ef6a01584aa381c14e26212daef115c5dcd31575
dimak <rahbussiness@gmail.com>
2023-10-05 20:54:37 +0200

    mod

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ use logging::logger::init_logger;
 
 use crate::handlers::entry::handle_cli;
 
+mod api;
 mod cmd;
 mod config;
 mod handlers;
 mod logging;
 mod models;
+mod utils;
 
 fn main() {
     init_logger();
