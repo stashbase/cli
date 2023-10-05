c78cea9bd5ed4b886ae5324e1e6b27d963518270
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-05 21:23:49 +0200

    post_request fn

--- src/api/client.rs
+++ src/api/client.rs
@@ -27,3 +27,31 @@ pub fn get_request(args: GetRequestArgs) -> RequestBuilder {
         .request(reqwest::Method::GET, full_path)
         .headers(headers)
 }
+
+pub fn post_request<T>(args: GetRequestArgs, data: Option<T>) -> RequestBuilder
+where
+    T: serde::Serialize,
+{
+    let base_path =
+        env::var("HERO_API_URL").unwrap_or_else(|_| format!("http://localhost:8080/api/v1/cli"));
+
+    let client = build_client();
+    let full_path = format!("{}/{}", base_path, args.path);
+
+    let mut headers = HeaderMap::new();
+    headers.insert("token", args.token.parse().unwrap());
+
+    match data {
+        Some(data) => {
+            headers.insert("Content-Type", "application/json".parse().unwrap());
+
+            client
+                .request(reqwest::Method::POST, full_path)
+                .headers(headers)
+                .json(&data)
+        }
+        None => client
+            .request(reqwest::Method::POST, full_path)
+            .headers(headers),
+    }
+}
