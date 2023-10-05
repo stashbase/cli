522bad4303d7ce4ebf56cce38e410c0c22608a53
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-05 21:23:57 +0200

    create_project api call

--- src/api/projects.rs
+++ src/api/projects.rs
@@ -1,6 +1,9 @@
 use reqwest::{Error, Response};
 
-use crate::models::api_client::{ApiPath, GetRequestArgs};
+use crate::models::{
+    api_client::{ApiPath, GetRequestArgs},
+    projects::CreateProjectPayload,
+};
 
 use super::client;
 
@@ -11,6 +14,17 @@ pub async fn list_projects(token: String) -> Result<Response, Error> {
     };
 
     let request = client::get_request(args);
+
+    return request.send().await;
+}
+
+pub async fn create_project(token: String, data: CreateProjectPayload) -> Result<Response, Error> {
+    let args = GetRequestArgs {
+        path: ApiPath::Projects(None),
+        token,
+    };
+
+    let request = client::post_request(args, Some(data));
 
     return request.send().await;
 }
