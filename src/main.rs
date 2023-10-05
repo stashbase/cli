c7685e1b032608e91c24c46348ed8f1f8de8d93d
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-05 16:33:44 +0200

    handle_cli

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ use cmd::root::Cli;
 use log::debug;
 use logging::logger::init_logger;
 
+use crate::handlers::entry::handle_cli;
+
 mod cmd;
+mod handlers;
 mod logging;
 
 fn main() {
     init_logger();
 
     let args = Cli::parse();
-
     debug!("Args: {:?}", args);
+
+    handle_cli(args);
 }
