48621bb75ddad9629496e677cb20d3067194ac92
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-06 14:31:01 +0200

    handle open project

---
+++ src/handlers/projects/open.rs
@@ -0,0 +1,56 @@
+use anyhow::{bail, Result};
+use log::error;
+use serde::Deserialize;
+
+use crate::{
+    api::projects, models::api_client::GetRequestApiResponse, utils::spinner::request_spinner,
+};
+
+#[derive(Debug, Deserialize)]
+struct OpenProjectResponse {
+    url: String,
+}
+
+pub async fn handle_open_project(token: String, name: String) -> Result<()> {
+    // send request
+    let mut spinner = request_spinner();
+    let project_res = projects::get_project_url(token, name).await;
+
+    if let Err(err) = project_res {
+        spinner.stop_and_persist("", "");
+        error!("{:#?}", &err);
+        bail!(err);
+    }
+
+    let project_res = project_res.unwrap();
+
+    match project_res {
+        GetRequestApiResponse::Ok(data) => {
+            let data = serde_json::from_str::<OpenProjectResponse>(&data.text);
+
+            match data {
+                Ok(data) => {
+                    let url = data.url;
+                    spinner.stop_with_message(&format!("Opening URL: {}", url));
+
+                    if let Err(err) = webbrowser::open(&url) {
+                        spinner.stop_with_message(&format!("Error opening URL: {}", err));
+                    }
+                }
+                Err(e) => {
+                    error!("{:#?}", e);
+                    bail!("Something went wrong");
+                }
+            }
+        }
+        GetRequestApiResponse::Err(e) => {
+            spinner.stop_with_message(&format!("{}", e));
+        }
+    }
+
+    // if let Err(err) = webbrowser::open(&url) {
+    //     eprintln!("Error opening URL: {}", err);
+    // }
+    //
+    Ok(())
+}
