e99f006827b09d135b4d15e50f1494e8a548d046
Radim Höfer <radim@stashbase.dev>
2023-10-06 11:20:59 +0200

    api errors

--- src/models/api_client.rs
+++ src/models/api_client.rs
@@ -1,5 +1,9 @@
 use core::fmt;
 
+use owo_colors::OwoColorize;
+use reqwest::StatusCode;
+use serde::Deserialize;
+
 #[derive(Debug)]
 pub struct GetRequestArgs {
     pub token: String,
@@ -19,5 +23,77 @@ impl fmt::Display for ApiPath {
                 None => write!(f, "projects"),
             },
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct GetRequestApiResponseOk {
+    pub status: StatusCode,
+    pub text: String,
+}
+
+#[derive(Debug)]
+pub enum GetRequestApiResponse {
+    Ok(GetRequestApiResponseOk),
+    Err(CustomError),
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiErrorResponse {
+    pub error: ApiError,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiError {
+    pub code: ApiErrorEntity,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum ApiErrorEntity {
+    Project(ProjectError),
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ProjectError {
+    InvalidName,
+    ProjectAlreadyExists,
+}
+
+#[derive(Debug)]
+pub struct CustomError {
+    pub message: String,
+    pub hint: Option<String>,
+}
+
+impl From<ApiError> for CustomError {
+    fn from(api_error: ApiError) -> Self {
+        match api_error.code {
+            ApiErrorEntity::Project(e) => match e {
+                ProjectError::InvalidName => CustomError {
+                    message: format!("Invalid name"),
+                    hint: None,
+                },
+                ProjectError::ProjectAlreadyExists => CustomError {
+                    message: format!("Project already exists"),
+                    hint: Some(format!("Try a different name")),
+                },
+            },
+        }
+    }
+}
+
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "{}", "Error".red().bold())?;
+
+        writeln!(f, "- message: {}", self.message).and_then(|_| {
+            if let Some(hint) = &self.hint {
+                writeln!(f, "- hint: {}", hint)
+            } else {
+                Ok(())
+            }
+        })
     }
 }
