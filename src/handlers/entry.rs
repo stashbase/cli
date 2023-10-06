23096cd47a7868de78ab2b8cdbec79d57de038fc
dimak <rahbussiness@gmail.com>
2023-10-06 15:47:51 +0200

    get project

--- src/handlers/entry.rs
+++ src/handlers/entry.rs
@@ -8,8 +8,8 @@ use crate::{
     },
     config::config,
     handlers::projects::{
-        create::handle_create_project, delete::handle_delete_project, list::handle_list_projects,
-        open::handle_open_project,
+        create::handle_create_project, delete::handle_delete_project, get::handle_get_project,
+        list::handle_list_projects, open::handle_open_project,
     },
     models::config::UpdateConfig,
 };
@@ -25,12 +25,24 @@ pub async fn handle_cli(args: Cli) {
         // TODO: check token for api commands
         let token = config.token.unwrap();
 
+        let raw_output = args.raw;
+
         match args.entity_type {
             EntityType::Project(cmd) => match cmd.subcommand {
                 ProjectSubcommand::List(_) => {
-                    handle_list_projects(token).await.unwrap_or_else(|err| {
-                        println!("{:?}", err);
-                    });
+                    handle_list_projects(token, raw_output)
+                        .await
+                        .unwrap_or_else(|err| {
+                            println!("{:?}", err);
+                        });
+                }
+
+                ProjectSubcommand::Get(args) => {
+                    handle_get_project(token, raw_output, args.name)
+                        .await
+                        .unwrap_or_else(|err| {
+                            eprintln!("{:?}", err);
+                        });
                 }
 
                 ProjectSubcommand::Create(args) => {
---
+++ src/handlers/projects/get.rs
@@ -0,0 +1,99 @@
+use anyhow::{bail, Result};
+use colored_json::to_colored_json_auto;
+use log::{debug, error};
+
+use crate::{
+    api::projects,
+    models::{api_client::GetRequestApiResponse, projects::ProjectWithCount},
+    utils::spinner::request_spinner,
+};
+
+pub async fn handle_get_project(token: String, raw: bool, name: String) -> Result<()> {
+    let mut spinner = request_spinner();
+    let project_res = projects::get_project(token, name).await;
+
+    if let Err(err) = project_res {
+        spinner.stop_and_persist("", "");
+        error!("{:#?}", &err);
+        bail!(err);
+    }
+
+    let project_res = project_res.unwrap();
+
+    match project_res {
+        GetRequestApiResponse::Ok(data) => {
+            debug!("{:#?}", &data.text);
+            spinner.stop_and_persist("", "");
+
+            let project = serde_json::from_str::<ProjectWithCount>(&data.text);
+
+            match project {
+                Ok(project) => {
+                    debug!("{:#?}", &project);
+
+                    if raw {
+                        let value = serde_json::to_value(&project).unwrap();
+                        let pretty = to_colored_json_auto(&value).unwrap();
+                        println!("{}", pretty);
+                    } else {
+                        print!("{}", project);
+                    }
+                }
+                Err(_) => {
+                    bail!("Something went wrong")
+                }
+            }
+        }
+        GetRequestApiResponse::Err(e) => {
+            // error!("{:#?}", &e);
+            // eprint!("{}", e);
+            spinner.stop_with_message(&format!("{}", e));
+        }
+    }
+
+    Ok(())
+    //
+    // let project_res = projects::list_projects(token).await;
+    // spinner.stop_and_persist("", "");
+    //
+    // if let Err(err) = &project_res {
+    //     error!("{:#?}", &err);
+    //     bail!("Could not connect to API")
+    // }
+    //
+    // let project_res = project_res.unwrap();
+    //
+    // let status = project_res.status();
+    //
+    // if status == 401 {
+    //     bail!("Unauthorized")
+    // }
+    //
+    // let response_text = project_res.text().await;
+    // debug!("{:#?}", &response_text);
+    //
+    // match response_text {
+    //     Ok(text) => {
+    //         let projects = serde_json::from_str::<Vec<Project>>(&text);
+    //
+    //         match projects {
+    //             Ok(projects) => {
+    //                 debug!("{:#?}", &projects);
+    //                 let value = serde_json::to_value(&projects).unwrap();
+    //                 let pretty = to_colored_json_auto(&value).unwrap();
+    //
+    //                 println!("{}", pretty);
+    //             }
+    //             Err(e) => {
+    //                 error!("{:#?}", &e);
+    //                 bail!("Something went wrong")
+    //             }
+    //         }
+    //     }
+    //     Err(err) => {
+    //         bail!("Could not parse response: {:?}", err);
+    //     }
+    // }
+    //
+    // Ok(())
+}
