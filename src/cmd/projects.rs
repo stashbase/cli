7b1a81dffe2aff258436bd398db09ede25600272
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-06 19:59:56 +0200

    UpdateProject cmd

--- src/cmd/projects.rs
+++ src/cmd/projects.rs
@@ -11,12 +11,19 @@ pub enum ProjectSubcommand {
     /// List projects
     #[clap(alias = "l")]
     List(ListProjects),
+
     /// Get project
     #[clap(alias = "g")]
     Get(GetProject),
+
     /// Create a new project
     #[clap(aliases = &["c", "new"])]
     Create(CreateProject),
+
+    /// Update project
+    #[clap(aliases = &["u", "upd"])]
+    Update(UpdateProject),
+
     /// Delete a project
     #[clap(aliases = &["d", "del"])]
     Delete(DeleteProject),
@@ -33,6 +40,20 @@ pub struct ListProjects {}
 pub struct CreateProject {
     /// Project name
     pub name: String,
+
+    /// Project description
+    #[arg(value_enum, short = 'd', long = "description")]
+    pub description: Option<String>,
+}
+
+#[derive(Debug, Args)]
+pub struct UpdateProject {
+    /// Project name
+    pub name: String,
+
+    /// New name
+    #[arg(value_enum, short = 'n', long = "name")]
+    pub new_name: Option<String>,
 
     /// Project description
     #[arg(value_enum, short = 'd', long = "description")]
