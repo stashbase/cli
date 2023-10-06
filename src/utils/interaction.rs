d64b1c2666ec3272321d994a2f45f4a445ecb39b
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-06 13:38:44 +0200

    dialoguer functions

---
+++ src/utils/interaction.rs
@@ -0,0 +1,29 @@
+use dialoguer::{theme::ColorfulTheme, Confirm, Input};
+
+pub fn confirm_opt(prompt: &str) -> Option<bool> {
+    Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt(prompt)
+        .interact_opt()
+        .unwrap_or_else(|_| None)
+}
+
+pub fn confirm_text(confirm_text: &str) -> String {
+    Input::with_theme(&ColorfulTheme::default())
+        .with_prompt(format!("Type '{}' to confirm", confirm_text))
+        .validate_with(|input: &String| -> Result<(), &str> {
+            if input == confirm_text {
+                Ok(())
+            } else {
+                Err("Input not contain match")
+            }
+        })
+        .interact()
+        .unwrap_or_else(|_| format!(""))
+}
+
+pub fn input(prompt: &str) -> String {
+    Input::with_theme(&ColorfulTheme::default())
+        .with_prompt(prompt)
+        .interact_text()
+        .unwrap_or_else(|_| "".to_string())
+}
