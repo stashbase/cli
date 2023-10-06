5f1232daa5beac860cd864fed759cdf02e5cdfad
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-06 20:00:01 +0200

    validation

--- src/utils/validation.rs
+++ src/utils/validation.rs
@@ -3,18 +3,30 @@ use regex::Regex;
 
 use crate::models::validation::{InputValidationError, ProjectInputValidationError};
 
-pub fn validate_project_name(value: &str) -> Result<()> {
+pub fn validate_project_name(value: &str, is_new_name: bool) -> Result<()> {
     if value.len() < 2 {
-        bail!(InputValidationError::Projects(
-            ProjectInputValidationError::NameTooShort
-        ));
+        if is_new_name == false {
+            bail!(InputValidationError::Projects(
+                ProjectInputValidationError::NewNameTooShort
+            ))
+        } else {
+            bail!(InputValidationError::Projects(
+                ProjectInputValidationError::NameTooShort
+            ));
+        }
     }
     let regex = Regex::new(r"^[a-zA-Z0-9-_]+$").unwrap();
 
     if !regex.is_match(value) {
-        bail!(InputValidationError::Projects(
-            ProjectInputValidationError::NameFormat
-        ))
+        if is_new_name == false {
+            bail!(InputValidationError::Projects(
+                ProjectInputValidationError::NameFormat
+            ))
+        } else {
+            bail!(InputValidationError::Projects(
+                ProjectInputValidationError::NewNameFormat
+            ))
+        }
     }
 
     Ok(())
