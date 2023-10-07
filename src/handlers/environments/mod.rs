a3083df992b063cc6d51fc7ed3cffdd2349a05f9
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-07 14:04:17 +0200

    handle set env lock

--- src/handlers/environments/mod.rs
+++ src/handlers/environments/mod.rs
@@ -2,3 +2,4 @@ pub mod create;
 pub mod get;
 pub mod list;
 pub mod open;
+pub mod set_lock;
---
+++ src/handlers/environments/set_lock.rs
@@ -0,0 +1,50 @@
+use anyhow::{bail, Result};
+use log::{debug, error};
+
+use crate::{
+    api::environments,
+    models::api_client::PostPatchRequestApiResponse,
+    utils::{spinner::request_spinner, validation::validate_project_name},
+};
+
+pub async fn handle_set_env_lock(
+    token: String,
+    project: String,
+    environment: String,
+    lock: bool,
+) -> Result<()> {
+    // TODO: check valid name
+    let project_name_is_valid = validate_project_name(&project, false);
+
+    if let Err(err) = project_name_is_valid {
+        bail!(err);
+    }
+
+    debug!("updating lock status...:");
+    let mut spinner = request_spinner();
+
+    let res = environments::set_lock(token, project, environment, lock).await;
+
+    if let Err(err) = res {
+        spinner.stop_and_persist("", "");
+        error!("{:#?}", &err);
+        bail!(format!("Error sending request: {}", err));
+    }
+
+    let res = res.unwrap();
+
+    match res {
+        PostPatchRequestApiResponse::Ok(_) => {
+            if lock == true {
+                spinner.stop_with_message("🔒 Enviroment locked!");
+            } else {
+                spinner.stop_with_message("🔓 Enviroment unlocked!");
+            }
+        }
+        PostPatchRequestApiResponse::Err(e) => {
+            spinner.stop_with_message(&format!("{}", e));
+        }
+    }
+
+    Ok(())
+}
