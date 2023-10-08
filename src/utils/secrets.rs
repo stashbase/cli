38872cc99615965dbf311100a1b903f932ca17d2
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-08 11:06:58 +0200

    format secrets util

---
+++ src/utils/secrets.rs
@@ -0,0 +1,51 @@
+use colored_json::to_colored_json_auto;
+
+use crate::{cmd::secrets::SecretsFromat, models::secrets::Secret};
+
+pub fn format_secrets(secrets: Vec<Secret>, format: &SecretsFromat) -> String {
+    match format {
+        SecretsFromat::List => {
+            let mut text_to_print = String::new();
+
+            for (i, p) in secrets.iter().enumerate() {
+                // is last
+                if i == secrets.len() - 1 {
+                    text_to_print.push_str(&format!("{}", p))
+                } else {
+                    text_to_print.push_str(&format!("{}\n", p))
+                }
+            }
+
+            text_to_print
+        }
+        SecretsFromat::Dotenv => {
+            let dotenv_string: String = secrets
+                .iter()
+                .enumerate()
+                .map(|(i, s)| {
+                    if let Some(descr) = &s.description {
+                        if i != secrets.len() - 1 {
+                            return format!("# {}\n{}={}\n", descr, s.key, s.value);
+                        } else {
+                            return format!("# {}\n{}={}", descr, s.key, s.value);
+                        }
+                    } else {
+                        if i != secrets.len() - 1 {
+                            return format!("{}={}\n", s.key, s.value);
+                        } else {
+                            return format!("{}={}", s.key, s.value);
+                        }
+                    }
+                })
+                .collect::<_>();
+
+            dotenv_string
+        }
+        SecretsFromat::Json => {
+            let value = serde_json::to_value(&secrets).unwrap();
+            let pretty = to_colored_json_auto(&value).unwrap();
+
+            pretty
+        }
+    }
+}
