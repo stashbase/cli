dcd00edfa78dcd0a4dab1a250c191aa780b97ed8
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-09 16:33:36 +0200

    create env args + secrets file

--- src/handlers/environments/create.rs
+++ src/handlers/environments/create.rs
@@ -1,5 +1,8 @@
+use std::path::Path;
+
 use anyhow::{bail, Result};
 use log::{debug, error};
+use owo_colors::OwoColorize;
 
 use crate::{
     api::environments,
@@ -8,23 +11,65 @@ use crate::{
     models::{
         api_client::PostPatchRequestApiResponse,
         environments::{CreatEnvironmentPayload, EnvType},
+        secrets::Secret,
     },
-    utils::{spinner::request_spinner, validation::validate_project_name},
+    utils::{
+        files::check_file_exists, secrets::read_dotenv_file, spinner::request_spinner,
+        validation::validate_project_name,
+    },
 };
 
-pub async fn handle_create_environment(
-    token: String,
-    project: String,
-    name: String,
-    env_type: EnvironmentType,
-    description: Option<String>,
-    open: bool,
-) -> Result<()> {
+pub struct HandleCreateEnvironmentArgs {
+    pub token: String,
+    pub project: String,
+    pub name: String,
+    pub env_type: EnvironmentType,
+    pub description: Option<String>,
+    pub open: bool,
+    pub file_path: Option<String>,
+}
+
+pub async fn handle_create_environment(args: HandleCreateEnvironmentArgs) -> Result<()> {
+    let HandleCreateEnvironmentArgs {
+        token,
+        project,
+        name,
+        env_type,
+        description,
+        file_path,
+        open,
+    } = args;
+
     // TODO: validate also env name
     let name_is_valid = validate_project_name(&name, false);
 
     if let Err(err) = name_is_valid {
         bail!(err);
+    }
+
+    let mut secrets: Option<Vec<Secret>> = None;
+
+    if let Some(file_path) = file_path {
+        let path = Path::new(&file_path);
+        let file_exists = check_file_exists(&path);
+
+        if !file_exists {
+            let err_msg = format!("{} {}", "Error reading file:".red(), "file does not exist");
+            bail!("{}", err_msg);
+        }
+
+        let secrets_res = read_dotenv_file(path);
+
+        match secrets_res {
+            Ok(values) => {
+                debug!("{:#?}", values);
+
+                secrets = Some(values);
+            }
+            Err(e) => {
+                bail!(format!("{} {}", "Error reading file:".red(), e));
+            }
+        }
     }
 
     debug!("creating project...:");
@@ -35,6 +80,7 @@ pub async fn handle_create_environment(
         name,
         description,
         env_type: environment_type,
+        secrets,
     };
 
     let mut spinner = request_spinner();
