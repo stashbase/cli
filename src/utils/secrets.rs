374d00523b6c84dd23a75981bc95fb885f705dfd
Radim Höfer <radim@stashbase.dev>
2023-10-09 10:20:38 +0200

    breakline

--- src/utils/secrets.rs
+++ src/utils/secrets.rs
@@ -13,7 +13,11 @@ pub fn format_secrets(secrets: Vec<Secret>, format: &SecretsFromat) -> String {
                 if i == secrets.len() - 1 {
                     text_to_print.push_str(&format!("{}", p))
                 } else {
-                    text_to_print.push_str(&format!("{}\n", p))
+                    if i != 0 && p.description.is_some() {
+                        text_to_print.push_str(&format!("\n{}\n", p))
+                    } else {
+                        text_to_print.push_str(&format!("{}\n", p))
+                    }
                 }
             }
 
