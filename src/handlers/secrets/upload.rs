6e6adcad8edafcc7366712316f3d534a17c633fb
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-09 12:37:04 +0200

    handle upload secrets

---
+++ src/handlers/secrets/upload.rs
@@ -0,0 +1,75 @@
+use std::path::Path;
+
+use anyhow::{bail, Result};
+use log::debug;
+use owo_colors::OwoColorize;
+
+use crate::{
+    api::secrets,
+    models::api_client::PostPatchRequestApiResponse,
+    utils::{secrets::read_dotenv_file, spinner::request_spinner},
+};
+
+pub struct HandleUploadSecretsArgs {
+    pub token: String,
+    pub project: String,
+    pub environment: String,
+    pub file_path: String,
+}
+
+pub async fn handle_upload_secrets(args: HandleUploadSecretsArgs) -> Result<()> {
+    let HandleUploadSecretsArgs {
+        token,
+        project,
+        environment,
+        file_path,
+    } = args;
+
+    let path = Path::new(&file_path);
+    debug!("Path: {:#?}", path);
+
+    let file_exists = path.exists();
+    debug!("File exists: {}", file_exists);
+
+    if !file_exists {
+        let err_msg = format!("{} {}", "Error reading file:".red(), "file does not exist");
+        bail!("{}", err_msg);
+    }
+
+    let secrets_res = read_dotenv_file(path);
+
+    match secrets_res {
+        Ok(secrets) => {
+            let mut spinner = request_spinner();
+            let res = secrets::set_sercrets(token, project, environment, secrets).await;
+            debug!("{:#?}", res);
+
+            if let Err(err) = res {
+                spinner.stop_and_persist("", "");
+                debug!("Error: {:#?}", &err);
+                bail!(err);
+            }
+
+            let res = res.unwrap();
+
+            match res {
+                PostPatchRequestApiResponse::Ok(_) => {
+                    spinner.stop_with_message(&format!(
+                        "{} {}",
+                        "✓".green(),
+                        "Secrets have been uploaded!"
+                    ));
+                }
+                PostPatchRequestApiResponse::Err(e) => {
+                    debug!("Error: {}", e);
+                    spinner.stop_with_message(&format!("\n{}", e));
+                }
+            }
+        }
+        Err(e) => {
+            bail!(format!("{} {}", "Error reading file:".red(), e));
+        }
+    }
+
+    Ok(())
+}
