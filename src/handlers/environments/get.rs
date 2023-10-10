ff5cad499b7b205e75d92cb8104a7be056b0b547
dimak <rahbussiness@gmail.com>
2023-10-10 17:42:37 +0200

    validate input

--- src/handlers/environments/get.rs
+++ src/handlers/environments/get.rs
@@ -5,10 +5,7 @@ use log::debug;
 use crate::{
     api::environments,
     models::{api_client::GetRequestApiResponse, environments::Environment},
-    utils::{
-        spinner::request_spinner,
-        validation::{validate_environment_name, validate_project_name},
-    },
+    utils::{spinner::request_spinner, validation::validate_project_environment},
 };
 
 pub async fn handle_get_environment(
@@ -17,18 +14,13 @@ pub async fn handle_get_environment(
     project: String,
     environment: String,
 ) -> Result<()> {
-    // TODO: check valid name
-    let project_name_is_valid = validate_project_name(&project, false);
+    let input_valid = validate_project_environment(&project, &environment);
 
-    if let Err(err) = project_name_is_valid {
+    if let Err(err) = input_valid {
         bail!(err);
     }
 
-    let env_name_is_valid = validate_environment_name(&environment);
-    if let Err(err) = env_name_is_valid {
-        bail!(err);
-    }
-
+    // OK
     debug!("getting env...");
 
     let mut spinner = request_spinner();
