c718e080c34c54f4c6303ed3456dfe581995ed5d
Radim Höfer <radim@stashbase.dev>
2023-10-10 19:45:36 +0200

    removed old code

--- src/handlers/projects/delete.rs
+++ src/handlers/projects/delete.rs
@@ -51,48 +51,4 @@ pub async fn handle_delete_project(token: String, name: String) -> Result<()> {
     }
 
     Ok(())
-    //
-    // let project_res = projects::list_projects(token).await;
-    // spinner.stop_and_persist("", "");
-    //
-    // if let Err(err) = &project_res {
-    //     error!("{:#?}", &err);
-    //     bail!("Could not connect to API")
-    // }
-    //
-    // let project_res = project_res.unwrap();
-    //
-    // let status = project_res.status();
-    //
-    // if status == 401 {
-    //     bail!("Unauthorized")
-    // }
-    //
-    // let response_text = project_res.text().await;
-    // debug!("{:#?}", &response_text);
-    //
-    // match response_text {
-    //     Ok(text) => {
-    //         let projects = serde_json::from_str::<Vec<Project>>(&text);
-    //
-    //         match projects {
-    //             Ok(projects) => {
-    //                 debug!("{:#?}", &projects);
-    //                 let value = serde_json::to_value(&projects).unwrap();
-    //                 let pretty = to_colored_json_auto(&value).unwrap();
-    //
-    //                 println!("{}", pretty);
-    //             }
-    //             Err(e) => {
-    //                 error!("{:#?}", &e);
-    //                 bail!("Something went wrong")
-    //             }
-    //         }
-    //     }
-    //     Err(err) => {
-    //         bail!("Could not parse response: {:?}", err);
-    //     }
-    // }
-    //
-    // Ok(())
 }
