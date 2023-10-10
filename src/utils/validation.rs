e7b7009811496d3050bfd04cc1d4835400adffa2
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-10 09:54:25 +0200

    validate secret keys

--- src/utils/validation.rs
+++ src/utils/validation.rs
@@ -1,7 +1,9 @@
 use anyhow::{bail, Result};
 use regex::Regex;
 
-use crate::models::validation::{InputValidationError, ProjectInputValidationError};
+use crate::models::validation::{
+    InputValidationError, ProjectInputValidationError, SecretsInputValidationError,
+};
 
 pub fn validate_project_name(value: &str, is_new_name: bool) -> Result<()> {
     if value.len() < 2 {
@@ -30,4 +32,35 @@ pub fn validate_project_name(value: &str, is_new_name: bool) -> Result<()> {
     }
 
     Ok(())
+}
+
+// TODO: validate env name
+
+// name of secret
+// pub fn validate_secret_key(value: &str) -> Result<()> {
+//     let regex = Regex::new(r"^[A-Z0-9_]+$").unwrap();
+//
+//     if !regex.is_match(value) {
+//         bail!(InputValidationError::Secrets(
+//             SecretsInputValidationError::KeyFormat { multiple: false }
+//         ));
+//     } else {
+//         Ok(())
+//     }
+// }
+
+pub fn validate_secret_keys(values: &Vec<String>) -> Result<()> {
+    let regex = Regex::new(r"^[A-Z0-9_]+$").unwrap();
+
+    let invalid = values.into_iter().find(|v| !regex.is_match(*v));
+
+    if invalid.is_some() {
+        let multiple = values.len() > 1;
+
+        bail!(InputValidationError::Secrets(
+            SecretsInputValidationError::KeyFormat { multiple }
+        ));
+    } else {
+        Ok(())
+    }
 }
