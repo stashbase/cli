e2333ea43aeb544e161aa0117f9a045805b3ce40
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-11 15:19:21 +0200

    print msg with spinner

--- src/handlers/environments/delete.rs
+++ src/handlers/environments/delete.rs
@@ -49,7 +49,7 @@ pub async fn handle_delete_environment(
         DeleteRequestApiResponse::Err(e) => {
             // error!("{:#?}", &e);
             // eprint!("{}", e);
-            spinner.stop_with_message(&format!("\n{}", e));
+            spinner.stop_with_message(&format!("{}", e));
         }
     }
 
