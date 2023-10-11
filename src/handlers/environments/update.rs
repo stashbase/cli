6c2350c2d7540ad4677ba4853921a5a5476a1152
Radim Höfer <radim@stashbase.dev>
2023-10-11 15:19:21 +0200

    print msg with spinner

--- src/handlers/environments/update.rs
+++ src/handlers/environments/update.rs
@@ -59,7 +59,7 @@ pub async fn handle_update_environment(
             spinner.stop_with_message("✏️ Environment has been updated!");
         }
         PostPatchRequestApiResponse::Err(e) => {
-            spinner.stop_with_message(&format!("\n{}", e));
+            spinner.stop_with_message(&format!("{}", e));
         }
     }
 
