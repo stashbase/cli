5f044f9f401b3ecbb2caaf606ab5b44fe890fe48
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-18 14:11:33 +0200

    colored environment formatter

--- src/models/environments.rs
+++ src/models/environments.rs
@@ -18,10 +18,14 @@ pub enum EnvType {
 impl Display for EnvType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            EnvType::DEVELOPMENT => write!(f, "Development"),
-            EnvType::TESTING => write!(f, "Testing"),
-            EnvType::STAGING => write!(f, "Staging"),
-            EnvType::PRODUCTION => write!(f, "Production"),
+            // EnvType::DEVELOPMENT => write!(f, "Development"),
+            // EnvType::TESTING => write!(f, "Testing"),
+            // EnvType::STAGING => write!(f, "Staging"),
+            // EnvType::PRODUCTION => write!(f, "Production"),
+            EnvType::DEVELOPMENT => write!(f, "{}", "Development".blue()),
+            EnvType::TESTING => write!(f, "{}", "Testing".cyan()),
+            EnvType::STAGING => write!(f, "{}", "Staging".green()),
+            EnvType::PRODUCTION => write!(f, "{}", "Production".red()),
         }
     }
 }
