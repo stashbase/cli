fa2a2c1a929295b78f27cdd7c258904708a1f0b0
Radim Höfer <radim@stashbase.dev>
2023-10-19 17:32:40 +0200

    revert change handler

---
+++ src/handlers/env_changelog/revert.rs
@@ -0,0 +1,85 @@
+use anyhow::{bail, Result};
+use log::debug;
+use owo_colors::OwoColorize;
+
+use crate::{
+    api::env_changelog,
+    models::api_client::PostPatchRequestApiResponse,
+    utils::{interaction, spinner::request_spinner, validation::validate_project_environment},
+};
+
+pub struct HandleRevertEnvChangelogChange {
+    pub token: String,
+    pub project: String,
+    pub environment: String,
+    pub change_id: String,
+}
+
+pub async fn handle_revert_changelog_change(args: HandleRevertEnvChangelogChange) -> Result<()> {
+    let HandleRevertEnvChangelogChange {
+        token,
+        project,
+        environment,
+        change_id,
+    } = args;
+
+    let input_valid = validate_project_environment(&project, &environment, true);
+
+    if let Err(err) = input_valid {
+        bail!(err);
+    }
+
+    let i = interaction::confirm_opt("Are you sure?");
+
+    if i.is_none() || (i.unwrap() == false) {
+        return Ok(());
+    }
+
+    // OK
+    debug!("reverting env changelog...");
+
+    let mut spinner = request_spinner();
+
+    let args = env_changelog::RevertArgs {
+        token,
+        project,
+        environment,
+        change_id,
+    };
+
+    let res = env_changelog::revert(args).await;
+
+    if let Err(err) = res {
+        spinner.stop_and_persist("", "");
+        debug!("Error: {:#?}", &err);
+        bail!(err);
+    }
+
+    let res = res.unwrap();
+
+    match res {
+        PostPatchRequestApiResponse::Ok(data) => {
+            debug!("{:#?}", &data.text);
+
+            // nothning to revert
+            if data.status == 200 {
+                spinner.stop_with_message(&format!(
+                    "\n{}",
+                    "Nothing to revert\n- details: this is current state"
+                ))
+            } else {
+                spinner.stop_with_message(&format!(
+                    "\n{} {}",
+                    "✓".green(),
+                    "Change has been reverted".green()
+                ));
+            }
+        }
+        PostPatchRequestApiResponse::Err(e) => {
+            debug!("Error: {:#?}", e);
+            spinner.stop_with_message(&format!("\n{}", e));
+        }
+    }
+
+    Ok(())
+}
