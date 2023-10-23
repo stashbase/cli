a3c7f833135ce4f5691b3c261b9d45ecea091a63
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-23 12:29:34 +0200

    command arg

--- src/cmd/load.rs
+++ src/cmd/load.rs
@@ -2,6 +2,9 @@ use clap::Args;
 
 #[derive(Debug, Args)]
 pub struct LoadCommand {
+    /// Command to run
+    pub command: String,
+
     /// Project name
     #[arg(value_enum, short = 'p', long = "project", required = true)]
     pub project: String,
