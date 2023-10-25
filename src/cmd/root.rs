bfdd93fd93c4584f035064894ccd6914c4128848
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-25 11:44:42 +0200

    color global arg - not used

--- src/cmd/root.rs
+++ src/cmd/root.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 use super::{
     configs::ConfigCommands, environments::EnvironmentCommands, projects::ProjectCommands,
@@ -14,10 +14,29 @@ pub struct Cli {
     #[arg(long = "raw", global = true)]
     pub raw: bool,
 
+    // #[clap(long, value_enum, global = true, default_value = "auto")]
+    // pub color: Color,
+    //
     #[clap(subcommand)]
     pub entity_type: EntityType,
 }
 
+// #[derive(ValueEnum, Clone, Copy, Debug)]
+// pub enum Color {
+//     Auto,
+//     Never,
+// }
+//
+// impl Color {
+//     pub fn init(self) {
+//         // Set a supports-color override based on the variable passed in.
+//         match self {
+//             Color::Auto => {}
+//             Color::Never => owo_colors::set_override(false),
+//         }
+//     }
+// }
+//
 #[derive(Debug, Subcommand)]
 pub enum EntityType {
     /// Load environment and run command
