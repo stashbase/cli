d64e936707ec6d9ba287200172f587099acea9b8
Radim Höfer <radim@stashbase.dev>
2023-10-28 13:09:12 +0200

    subprocess running static mutext - ctrlc use

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::sync::Mutex;
+
 use clap::Parser;
 use cmd::root::Cli;
 use log::debug;
 use logging::logger::init_logger;
+use once_cell::sync::Lazy; // 1.3.1
 
 use crate::handlers::entry::root::handle_cli;
 
@@ -12,6 +15,8 @@ mod handlers;
 mod logging;
 mod models;
 mod utils;
+
+pub static SUBPROCESS_RUNNING: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
 
 fn main() {
     init_logger();
@@ -26,14 +31,15 @@ fn main() {
 fn set_handlers() {
     // https://github.com/console-rs/dialoguer/issues/77
     ctrlc::set_handler(move || {
-        let term = dialoguer::console::Term::stdout();
-        let _ = term.show_cursor();
+        if *SUBPROCESS_RUNNING.lock().unwrap() != true {
+            let term = dialoguer::console::Term::stdout();
+            let _ = term.show_cursor();
 
-        eprintln!("");
-        // exit process
-        // exits also subprocess if using run command
-        //  TODO: check if child process is running -> dont exit
-        // std::process::exit(0);
+            eprintln!("");
+
+            // exit process
+            std::process::exit(0);
+        }
     })
     .expect("Error setting Ctrl-C handler");
 }
