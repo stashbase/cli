bfdc33deeac6de33fec21166b411897779c7b460
Radim Höfer <radim.engineering@stashbase.dev>
2023-12-20 14:13:46 +0100

    args

--- src/api/env_changelog.rs
+++ src/api/env_changelog.rs
@@ -11,8 +11,9 @@ pub struct ListArgs {
     pub project: String,
     pub environment: String,
     pub page: Option<usize>,
-    pub show_secrets: bool,
-    pub only_secrets: bool,
+    pub show_values: bool,
+    // pub show_secrets: bool,
+    // pub only_secrets: bool,
 }
 
 pub async fn list(args: ListArgs) -> Result<GetRequestApiResponse> {
@@ -20,21 +21,20 @@ pub async fn list(args: ListArgs) -> Result<GetRequestApiResponse> {
         token,
         project,
         environment,
-        show_secrets,
-        only_secrets,
+        show_values,
         page,
     } = args;
 
     let mut query = vec![];
 
-    if show_secrets == true {
-        query.push(("hidden".to_string(), "true".to_string()));
+    if show_values == true {
+        query.push(("hidden".to_string(), "false".to_string()));
     }
 
-    if only_secrets == true {
-        query.push(("only-secrets".to_string(), "true".to_string()));
-    }
-
+    // if only_secrets == true {
+    //     query.push(("only-secrets".to_string(), "true".to_string()));
+    // }
+    //
     if let Some(page) = page {
         query.push(("page".to_string(), page.to_string()));
     }
