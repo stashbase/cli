2d5dba9642f9339c120cfa33d01f45ee61f111e5
Radim Höfer <radim@stashbase.dev>
2023-12-28 11:02:09 +0100

    handle open cmd

--- src/handlers/open.rs
+++ src/handlers/open.rs
@@ -1,10 +1,55 @@
-pub fn handle_open_dashboard() {
-    // TODO: get workspace id
-    let url = "http://localhost:3000/workspace/4ef8a291-024e-4ed8-924b-1cc90d01315e/projects";
+use anyhow::{bail, Result};
+use log::error;
+use serde::Deserialize;
 
-    eprintln!("Opening URL: {}", url);
+use crate::{
+    api::workspace, models::api_client::GetRequestApiResponse, utils::spinner::request_spinner,
+};
 
-    if let Err(err) = webbrowser::open(&url) {
-        eprintln!("{}", &format!("Error opening URL: {}", err));
+#[derive(Debug, Deserialize)]
+struct OpeDashboardResponse {
+    url: String,
+}
+
+pub async fn handle_open_dashboard(token: String) -> Result<()> {
+    let mut spinner = request_spinner();
+
+    let response = workspace::get_url(token).await;
+
+    if let Err(err) = response {
+        spinner.stop_and_persist("", "");
+        error!("{:#?}", &err);
+        bail!(err);
     }
+
+    let project_res = response.unwrap();
+
+    match project_res {
+        GetRequestApiResponse::Ok(data) => {
+            let data = serde_json::from_str::<OpeDashboardResponse>(&data.text);
+
+            match data {
+                Ok(data) => {
+                    let url = data.url;
+                    spinner.stop_with_message(&format!("Opening URL: {}", url));
+
+                    if let Err(err) = webbrowser::open(&url) {
+                        spinner.stop_with_message(&format!("Error opening URL: {}", err));
+                    }
+                }
+                Err(e) => {
+                    error!("{:#?}", e);
+                    bail!("Something went wrong");
+                }
+            }
+        }
+        GetRequestApiResponse::Err(e) => {
+            spinner.stop_with_message(&format!("{}", e));
+        }
+    }
+
+    // if let Err(err) = webbrowser::open(&url) {
+    //     eprintln!("{}", &format!("Error opening URL: {}", err));
+    // }
+    Ok(())
 }
