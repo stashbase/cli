c94b44376e7145a4bc1b6e6f7bfc5169148dfeec
Radim Höfer <radim.engineering@stashbase.dev>
2024-02-24 21:26:38 +0100

    feat: rotate webhook secret

---
+++ src/handlers/webhooks/rotate_secret.rs
@@ -0,0 +1,61 @@
+use log::debug;
+
+use anyhow::{bail, Result};
+
+use crate::{
+    api::webhooks,
+    models::{api_client::PostPatchRequestApiResponse, webhooks::RotateWebhookSecretResponse},
+    utils::{interaction, spinner::request_spinner},
+};
+
+pub type RotateWebhookSecretArgs = webhooks::RotateArgs;
+
+pub async fn handle_rotate_webhook_secret(args: RotateWebhookSecretArgs) -> Result<()> {
+    let i = interaction::confirm_opt("Are you sure you want to rotate signing secret?");
+
+    if i.is_none() || (i.unwrap() == false) {
+        return Ok(());
+    }
+
+    let mut spinner = request_spinner();
+
+    let res = webhooks::rotate_secret(args).await;
+
+    if let Err(err) = res {
+        spinner.stop_and_persist("", "");
+        debug!("Error: {:#?}", &err);
+        bail!(err);
+    }
+
+    // safe
+    let res = res.unwrap();
+
+    match res {
+        PostPatchRequestApiResponse::Ok(res_data) => {
+            if let Some(res_text) = res_data.text {
+                let data = serde_json::from_str::<RotateWebhookSecretResponse>(&res_text);
+
+                match data {
+                    Ok(data) => {
+                        spinner.stop_with_message("⟳ Webhook secret has been rotated!");
+                        // spinner.stop_with_message("✅ Webhook secret has been rotated!");
+                        println!("\nSigning secret: {}", &data.signing_secret);
+                    }
+                    Err(e) => {
+                        spinner.stop_and_persist("", "");
+                        debug!("Err: {}", e);
+                        bail!("Something went wrong")
+                    }
+                }
+            } else {
+                panic!();
+            }
+        }
+        PostPatchRequestApiResponse::Err(e) => {
+            // eprintln!("{}", e);
+            spinner.stop_with_message(&format!("\n{}", e));
+        }
+    }
+
+    Ok(())
+}
