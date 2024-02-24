a334329ec5201ae719303436e0a81704c2bf2e7e
dimak <rahbussiness@gmail.com>
2024-02-24 14:28:54 +0100

    add: struct UpdateWebhookStatusPayload

--- src/models/webhooks.rs
+++ src/models/webhooks.rs
@@ -91,16 +91,22 @@ pub struct CreateWebhookResponse {
 }
 
 // update
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize)]
 pub struct UpdateWebhookPayload {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub url: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
+    // NOTE: created separate
+    // #[serde(skip_serializing_if = "Option::is_none")]
+    // pub enabled: Option<bool>,
+}
 
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub enabled: Option<bool>,
+// update
+#[derive(Debug, Serialize)]
+pub struct UpdateWebhookStatusPayload {
+    pub enabled: bool,
 }
 
 // test
