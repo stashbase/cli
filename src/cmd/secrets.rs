896694d26cffde8a873114d512f9e26e73ab0daa
dimak <rahbussiness@gmail.com>
2024-04-25 21:07:39 +0200

    refactor: shared args at root + subcommand

--- src/cmd/secrets.rs
+++ src/cmd/secrets.rs
@@ -1,17 +1,113 @@
+use anyhow::Result;
 use clap::{Args, Subcommand, ValueEnum};
 
 #[derive(Debug, Args)]
 pub struct SecretArgs {
     /// Project name
-    #[arg(value_enum, short = 'p', long = "project", required = true)]
-    pub project: String,
+    #[arg(value_enum, short = 'p', long = "project", required = false)]
+    pub project: Option<String>,
 
     /// Environment name
-    #[arg(value_enum, short = 'e', long = "environment", required = true)]
-    pub environment: String,
+    #[arg(value_enum, short = 'e', long = "environment", required = false)]
+    pub environment: Option<String>,
 
     #[clap(subcommand)]
     pub subcommand: SecretSubcommand,
+}
+
+// TODO: error
+impl SecretArgs {
+    pub fn try_get_project_environment(&self) -> Result<(String, String)> {
+        let root_project: Option<_> = self.project.as_deref();
+        let root_environment: Option<_> = self.environment.as_deref();
+
+        let (project, environment) = self.subcommand.get_project_environment();
+
+        if root_project.is_some() && project.is_some() {
+            return Err(anyhow::anyhow!("cannot use --project twice"));
+        }
+
+        if root_environment.is_some() && environment.is_some() {
+            return Err(anyhow::anyhow!("cannot use --environment twice"));
+        }
+
+        if project.is_none()
+            && root_project.is_none()
+            && environment.is_none()
+            && root_environment.is_none()
+        {
+            return Err(anyhow::anyhow!(
+                "must use -p/--project and -e/--environment arguments"
+            ));
+        }
+
+        if project.is_none() && root_project.is_none() {
+            return Err(anyhow::anyhow!("must use -p/--project argument"));
+        }
+
+        if environment.is_none() && root_environment.is_none() {
+            return Err(anyhow::anyhow!("must use -e/--environment argument"));
+        }
+
+        let project = match root_project {
+            Some(p) => p.to_string(),
+            None => project.unwrap(),
+        };
+
+        let environment = match root_environment {
+            Some(e) => e.to_string(),
+            None => environment.unwrap(),
+        };
+
+        return Ok((project, environment));
+    }
+}
+
+impl SecretSubcommand {
+    pub fn get_project_environment(&self) -> (Option<String>, Option<String>) {
+        match &self {
+            SecretSubcommand::List(l) => (
+                l.shared_args.project.to_owned(),
+                l.shared_args.environment.to_owned(),
+            ),
+            SecretSubcommand::Get(g) => (
+                g.shared_args.project.to_owned(),
+                g.shared_args.environment.to_owned(),
+            ),
+            SecretSubcommand::Set(s) => (
+                s.shared_args.project.to_owned(),
+                s.shared_args.environment.to_owned(),
+            ),
+            SecretSubcommand::Upload(u) => (
+                u.shared_args.project.to_owned(),
+                u.shared_args.environment.to_owned(),
+            ),
+            SecretSubcommand::Rename(r) => (
+                r.shared_args.project.to_owned(),
+                r.shared_args.environment.to_owned(),
+            ),
+            SecretSubcommand::Description(d) => (
+                d.shared_args.project.to_owned(),
+                d.shared_args.environment.to_owned(),
+            ),
+            SecretSubcommand::Delete(d) => (
+                d.shared_args.project.to_owned(),
+                d.shared_args.environment.to_owned(),
+            ),
+        }
+    }
+}
+
+/// Common options for listing secrets
+#[derive(Debug, Args)]
+pub struct SharedArgs {
+    /// Project name
+    #[arg(short = 'p', long = "project", required = false, hide = true)]
+    pub project: Option<String>,
+
+    /// Environment name
+    #[arg(short = 'e', long = "environment", required = false, hide = true)]
+    pub environment: Option<String>,
 }
 
 #[derive(Debug, Subcommand)]
@@ -47,6 +143,9 @@ pub enum SecretSubcommand {
 
 #[derive(Debug, Args)]
 pub struct ListSecrets {
+    #[clap(flatten)]
+    pub shared_args: SharedArgs,
+
     /// Search key
     #[arg(value_enum, long = "search")]
     pub search: Option<String>,
@@ -62,6 +161,9 @@ pub struct ListSecrets {
 
 #[derive(Debug, Args)]
 pub struct GetSecrets {
+    #[clap(flatten)]
+    pub shared_args: SharedArgs,
+
     // #[clap(short='v', long="k", value_parser, num_args = 1.., value_delimiter = ' ')]
     pub keys: Vec<String>,
 
@@ -72,6 +174,9 @@ pub struct GetSecrets {
 
 #[derive(Debug, Args)]
 pub struct DeleteSecrets {
+    #[clap(flatten)]
+    pub shared_args: SharedArgs,
+
     /// Secrets (keys) to delete
     #[clap(value_parser, num_args = 1.., value_delimiter = ' ')]
     pub keys: Vec<String>,
@@ -83,6 +188,9 @@ pub struct DeleteSecrets {
 
 #[derive(Debug, Args)]
 pub struct SetSecrets {
+    #[clap(flatten)]
+    pub shared_args: SharedArgs,
+
     /// Secrets to set: KEY_1=VAL_1 KEY_2=VAL_2
     #[clap(value_parser, num_args = 1..)]
     pub secrets: Vec<String>,
@@ -95,6 +203,9 @@ pub struct SetSecrets {
 
 #[derive(Debug, Args)]
 pub struct UploadSecrets {
+    #[clap(flatten)]
+    pub shared_args: SharedArgs,
+
     // NOTE: for now only accepts .env
     /// Path to file (dotenv format)
     pub file_path: String,
@@ -102,6 +213,9 @@ pub struct UploadSecrets {
 
 #[derive(Debug, Args)]
 pub struct SetDescription {
+    #[clap(flatten)]
+    pub shared_args: SharedArgs,
+
     /// Secret key
     pub key: String,
 
@@ -111,6 +225,9 @@ pub struct SetDescription {
 
 #[derive(Debug, Args)]
 pub struct RenameSecrets {
+    #[clap(flatten)]
+    pub shared_args: SharedArgs,
+
     /// Secrets to rename: KEY_1=NEW_KEY_1 KEY_2=NEW_KEY_2
     #[clap(value_parser, num_args = 1..)]
     pub secrets: Vec<String>,
