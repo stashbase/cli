f10d11f6e132e24fc81f22af3e2ee5a1e7f9348c
Radim Höfer <radim@stashbase.dev>
2024-06-07 13:04:25 +0200

    feat: handle set output command

--- src/handlers/config/set.rs
+++ src/handlers/config/set.rs
@@ -1,17 +1,32 @@
 // use anyhow::Result;
 use owo_colors::OwoColorize;
 
-use crate::{config::config, models::config::UpdateConfig};
+use crate::{cmd::configs::OutputFormat, config::config, models::config::UpdateConfig};
 
 pub fn set_api_key(api_key: String) {
     let res = config::update_config(UpdateConfig {
         api_key: Some(api_key),
+        output_format: None,
     });
 
     if let Err(err) = res {
         eprintln!("{} {}", "Error:".red(), err);
     } else {
         let msg = format!("{} {}", "✔".green(), "API Key has been set");
+        eprintln!("{}", msg);
+    }
+}
+
+pub fn set_default_output_format(output_format: OutputFormat) {
+    let res = config::update_config(UpdateConfig {
+        api_key: None,
+        output_format: Some(output_format),
+    });
+
+    if let Err(err) = res {
+        eprintln!("{} {}", "Error:".red(), err);
+    } else {
+        let msg = format!("{} {}", "✔".green(), "Default output format has been set");
         eprintln!("{}", msg);
     }
 }
