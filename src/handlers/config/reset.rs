eae830cad2de0044c9d774f1e90c5ee559a1297a
Radim Höfer <radim.engineering@stashbase.dev>
2024-06-09 20:44:07 +0200

    feat: reset config handler

---
+++ src/handlers/config/reset.rs
@@ -0,0 +1,19 @@
+use crate::{
+    config::config::{create_config, get_config_path},
+    utils::interaction,
+};
+use anyhow::Result;
+
+pub fn reset_config() -> Result<()> {
+    eprintln!("\nContent of the config file will be lost.");
+    let i = interaction::confirm_opt("Are you sure?");
+
+    if i.is_none() || (i.unwrap() == false) {
+        return Ok(());
+    }
+
+    let config_path = get_config_path()?;
+    create_config(config_path.as_path())?;
+
+    Ok(())
+}
