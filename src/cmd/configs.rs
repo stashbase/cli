774346720d4e5558a322e699d8fbc43920dd96fd
dimak <rahbussiness@gmail.com>
2024-06-09 17:24:42 +0200

    refactor: config cmd entity at root

--- src/cmd/configs.rs
+++ src/cmd/configs.rs
@@ -1,44 +1,6 @@
 use clap::{Args, Subcommand, ValueEnum};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Args)]
-#[command(override_usage = "config <COMMAND> [OPTIONS]")]
-pub struct ConfigCommands {
-    #[clap(subcommand)]
-    pub subcommand: ConfigSubcommand,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum ConfigSubcommand {
-    /// Set values
-    #[clap(alias = "s")]
-    Set(SetConfig),
-}
-
-#[derive(Debug, Args)]
-#[command(override_usage = "config set <COMMAND> [OPTIONS]")]
-pub struct SetConfig {
-    #[clap(subcommand)]
-    pub subcommand: SetConfigSubcommand,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum SetConfigSubcommand {
-    #[clap(alias = "t")]
-    /// Set api_key
-    ApiKey(SetApiKey),
-    /// Set default output format
-    Output(SetOutput),
-    /// Set default output format for secrets
-    OutputSecrets(SetOutputSecrets),
-}
-
-#[derive(Debug, Args)]
-#[command(override_usage = "config set api-key <VALUE> [OPTIONS]")]
-pub struct SetApiKey {
-    pub value: String,
-}
-
 #[derive(Debug, ValueEnum, Clone, PartialEq, Eq, Default, Serialize, Deserialize)]
 pub enum OutputFormat {
     #[default]
@@ -57,13 +19,131 @@ pub enum SecretsOutputFormat {
 }
 
 #[derive(Debug, Args)]
+#[command(override_usage = "config <COMMAND> [OPTIONS]")]
+pub struct ConfigCommands {
+    #[clap(subcommand)]
+    pub subcommand: ConfigSubcommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ConfigSubcommand {
+    /// Api key config
+    ApiKey(ApiKeyCommand),
+
+    Output(OutputCommand),
+    OutputSecrets(SecretsOutputCommand),
+}
+
+#[derive(Debug, Args)]
+pub struct ApiKeyCommand {
+    #[clap(subcommand)]
+    pub subcommand: ApiKeySubcommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ApiKeySubcommand {
+    /// Set api_key
+    #[clap(alias = "t")]
+    Set(SetApiKey),
+}
+
+#[derive(Debug, Args)]
+#[command(override_usage = "config set api-key <VALUE> [OPTIONS]")]
+pub struct SetApiKey {
+    pub value: String,
+}
+
+//
+
+#[derive(Debug, Args)]
+pub struct OutputCommand {
+    #[clap(subcommand)]
+    pub subcommand: OutputSubcommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum OutputSubcommand {
+    /// Set api_key
+    #[clap(alias = "t")]
+    Set(SetOutput),
+}
+
+#[derive(Debug, Args)]
 #[command(override_usage = "config set output <FORMAT> [OPTIONS]")]
 pub struct SetOutput {
     pub format: OutputFormat,
 }
 
+//
+
+#[derive(Debug, Args)]
+pub struct SecretsOutputCommand {
+    #[clap(subcommand)]
+    pub subcommand: SecretsOutputSubcommand,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum SecretsOutputSubcommand {
+    /// Set api_key
+    #[clap(alias = "t")]
+    Set(SetSecretsOutput),
+}
+
 #[derive(Debug, Args)]
 #[command(override_usage = "config set output-secrets <FORMAT> [OPTIONS]")]
-pub struct SetOutputSecrets {
+pub struct SetSecretsOutput {
     pub format: SecretsOutputFormat,
 }
+
+// #[derive(Debug, Args)]
+// #[command(override_usage = "config set <COMMAND> [OPTIONS]")]
+// pub struct SetConfig {
+//     #[clap(subcommand)]
+//     pub subcommand: SetConfigSubcommand,
+// }
+//
+// #[derive(Debug, Subcommand)]
+// pub enum SetConfigSubcommand {
+//     #[clap(alias = "t")]
+//     /// Set api_key
+//     ApiKey(SetApiKey),
+//     /// Set default output format
+//     Output(SetOutput),
+//     /// Set default output format for secrets
+//     OutputSecrets(SetOutputSecrets),
+// }
+//
+// #[derive(Debug, Args)]
+// #[command(override_usage = "config set api-key <VALUE> [OPTIONS]")]
+// pub struct SetApiKey {
+//     pub value: String,
+// }
+//
+// #[derive(Debug, ValueEnum, Clone, PartialEq, Eq, Default, Serialize, Deserialize)]
+// pub enum OutputFormat {
+//     #[default]
+//     List,
+//     Table,
+//     Json,
+// }
+//
+// #[derive(Debug, ValueEnum, Clone, PartialEq, Eq, Default, Serialize, Deserialize)]
+// pub enum SecretsOutputFormat {
+//     #[default]
+//     List,
+//     Dotenv,
+//     Table,
+//     Json,
+// }
+//
+// #[derive(Debug, Args)]
+// #[command(override_usage = "config set output <FORMAT> [OPTIONS]")]
+// pub struct SetOutput {
+//     pub format: OutputFormat,
+// }
+//
+// #[derive(Debug, Args)]
+// #[command(override_usage = "config set output-secrets <FORMAT> [OPTIONS]")]
+// pub struct SetOutputSecrets {
+//     pub format: SecretsOutputFormat,
+// }
