22ab4e4b0e047e164b1355ff378a6553587fea93
Radim Höfer <radim.engineering@stashbase.dev>
2024-06-12 13:34:24 +0200

    feat: handle unset state subcommand

--- src/handlers/entry/state.rs
+++ src/handlers/entry/state.rs
@@ -12,7 +12,7 @@ use crate::{
 
 use crate::{config::config, models::config::UpdateConfig};
 
-pub fn handle_state_commands(cmd: StateCommand, config: &Config) -> Result<()> {
+pub fn handle_state_commands(cmd: StateCommand, config: &mut Config) -> Result<()> {
     match cmd.subcommand {
         StateSubcommand::Set(cmd) => {
             if cmd.project.is_none() && cmd.environment.is_none() {
@@ -48,6 +48,25 @@ pub fn handle_state_commands(cmd: StateCommand, config: &Config) -> Result<()> {
                 }
             }
         }
+        StateSubcommand::Unset(cmd) => {
+            if cmd.project == false && cmd.environment == false {
+                bail!(InputValidationError::CmdArgs(
+                    CmdArgInputValidationError::MissingProjectOrEnvironment
+                ))
+            }
+
+            if let Some(state) = &config.state {
+                handle_unset_state(
+                    config,
+                    state.project.clone(),
+                    state.environment.clone(),
+                    cmd.project,
+                    cmd.environment,
+                );
+            } else {
+                eprintln!("{}", "No state to unset");
+            }
+        }
     }
 
     Ok(())
@@ -69,6 +88,32 @@ fn handle_set_state(project: Option<String>, environment: Option<String>) {
         eprintln!("{} {}", "Error:".red(), err);
     } else {
         let msg = format!("{} {}", "✔".green(), "State has been set");
+        eprintln!("{}", msg);
+    }
+}
+
+fn handle_unset_state(
+    config: &mut Config,
+    state_project: Option<String>,
+    state_environment: Option<String>,
+    unset_project: bool,
+    unset_environment: bool,
+) {
+    let updated_state = State {
+        project: if unset_project { None } else { state_project },
+        environment: if unset_environment {
+            None
+        } else {
+            state_environment
+        },
+    };
+
+    let res = config::update_config_state(config, Some(updated_state));
+
+    if let Err(err) = res {
+        eprintln!("{} {}", "Error:".red(), err);
+    } else {
+        let msg = format!("{} {}", "✔".green(), "State has been updated");
         eprintln!("{}", msg);
     }
 }
