0f049daa94c3b4035957346c408a5ebe8349c745
Radim Höfer <radim.engineering@stashbase.dev>
2024-06-28 18:43:01 +0200

    fix: condition - display method for State

--- src/models/config.rs
+++ src/models/config.rs
@@ -65,7 +65,7 @@ impl State {
 
 impl Display for State {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.project.is_some() && self.environment.is_some() {
+        if self.project.is_some() || self.environment.is_some() {
             //
             let mut text = "".to_string();
 
