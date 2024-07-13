b6d2013c394677b03121938cda2bf9c34b8c7815
Radim Höfer <radim@stashbase.dev>
2024-07-13 09:52:54 +0200

    feat: add cmd arg "resolve_refs" - pull cmd

--- src/cmd/pull.rs
+++ src/cmd/pull.rs
@@ -60,6 +60,10 @@ pub struct PullCommand {
     #[clap(value_parser, long="set", num_args = 1..)]
     pub set: Vec<String>,
 
+    /// Interpolate refereces to other secrets
+    #[arg(value_enum, long = "resolve-refs")]
+    pub resolve_refs: Option<bool>,
+
     /// Print loaded secrets
     #[arg(value_enum, long = "print")]
     pub print_secrets: bool,
