2e18dfcc92fb71bf3392db49db39d8ed26adb221
dimak <rahbussiness@gmail.com>
2024-07-13 09:53:54 +0200

    feat: add property "resolve_refs" to EnvConfigItemSecrets

--- src/models/config_env.rs
+++ src/models/config_env.rs
@@ -30,6 +30,9 @@ pub struct EnvConfigItemSecrets {
     // Exclude secret keys
     pub exclude: Option<Vec<String>>,
     pub set: Option<HashMap<String, String>>,
+
+    #[serde(rename = "resolveRefs")]
+    pub resolve_refs: Option<bool>,
 }
 
 impl fmt::Display for EnvConfigItem {
