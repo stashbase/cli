82f52a74b26cd40a0f09762b5317b5942793fc7f
dimak <rahbussiness@gmail.com>
2024-07-21 10:15:48 +0200

    feat: upload secrets using different formats

--- src/handlers/secrets/upload.rs
+++ src/handlers/secrets/upload.rs
@@ -6,18 +6,16 @@ use owo_colors::OwoColorize;
 
 use crate::{
     api::secrets,
+    cmd::secrets::SecretsFileFormat,
     models::{
         api_client::PostPatchRequestApiResponse,
         validation::{InputValidationError, SecretsInputValidationError},
     },
     utils::{
         interaction,
-        secrets::{find_duplicate_keys, read_dotenv_file},
+        secrets::{find_duplicate_keys, read_secrets_from_file},
         spinner::request_spinner,
-        validation::{
-            validate_project_environment, validate_secrets_references,
-            validate_secrets_references_with_existence,
-        },
+        validation::{validate_project_environment, validate_secrets_references_with_existence},
     },
 };
 
@@ -26,6 +24,7 @@ pub struct HandleUploadSecretsArgs {
     pub project: String,
     pub environment: String,
     pub file_path: String,
+    pub format: Option<SecretsFileFormat>,
 }
 
 pub async fn handle_upload_secrets(args: HandleUploadSecretsArgs) -> Result<()> {
@@ -34,6 +33,7 @@ pub async fn handle_upload_secrets(args: HandleUploadSecretsArgs) -> Result<()> 
         project,
         environment,
         file_path,
+        format,
     } = args;
 
     let proj_env_validation_res = validate_project_environment(&project, &environment, false);
@@ -53,125 +53,142 @@ pub async fn handle_upload_secrets(args: HandleUploadSecretsArgs) -> Result<()> 
         bail!("{}", err_msg);
     }
 
-    let secrets_res = read_dotenv_file(path);
-
-    match secrets_res {
-        Ok(secrets) => {
-            let duplicate_keys = find_duplicate_keys(&secrets);
-
-            if !duplicate_keys.is_empty() {
-                let err = InputValidationError::Secrets(
-                    SecretsInputValidationError::DuplicateKeys(duplicate_keys),
-                );
-
-                bail!("{}", err);
-            }
-
-            let refs_validation = validate_secrets_references_with_existence(&secrets);
-
-            if !refs_validation.self_referenced_secrets.is_empty() {
-                let err =
-                    InputValidationError::Secrets(SecretsInputValidationError::SelfReferences(
-                        refs_validation.self_referenced_secrets,
-                    ));
-                bail!(err);
-            } else if !refs_validation.invalid_format.is_empty()
-                || !refs_validation.not_found.is_empty()
-            {
-                let mut print_str = String::new();
-
-                if !refs_validation.invalid_format.is_empty() {
-                    let hint_str = refs_validation
-                        .invalid_format
-                        .iter()
-                        .map(|(k, v)| format!("{} ({})", k, v.join(", ")))
-                        .collect::<Vec<_>>()
-                        .join(", ");
-
-                    print_str.push_str(&format!("- message: invalid secret references format\n"));
-                    print_str.push_str(&format!("- secrets: {} \n", hint_str));
-                }
-
-                if !refs_validation.not_found.is_empty() {
-                    let hint_str = refs_validation
-                        .not_found
-                        .iter()
-                        .map(|(k, v)| format!("{} ({})", k, v.join(", ")))
-                        .collect::<Vec<_>>()
-                        .join(", ");
-
-                    if !print_str.is_empty() {
-                        print_str.push_str(&format!("\n"));
-                    }
-
-                    print_str.push_str(&format!(
-                        "- message: referenced secrets not found within the file\n"
-                    ));
-                    print_str.push_str(&format!("- secret: {} \n", hint_str));
-                }
-
-                if !refs_validation.invalid_format.is_empty()
-                    && !refs_validation.not_found.is_empty()
-                {
-                    eprintln!("{}", format!("{}", "Input warnings").yellow());
-                } else {
-                    eprintln!("{}", format!("{}", "Input warning").yellow());
-                }
-                eprintln!("{}", print_str);
-
-                let confirm = interaction::confirm_opt("Are you sure you want to continue?");
-
-                if confirm.is_none() || (confirm.unwrap() == false) {
-                    return Ok(());
-                }
-
-                // let hint_str = references_validation
-                //     .invalid_format_references
-                //     .iter()
-                //     .map(|(k, v)| format!("{} ({})", k, v.join(", ")))
-                //     .collect::<Vec<_>>()
-                //     .join(", ");
-                //
-                // eprintln!("{}", format!("{}", "Input warning").yellow());
-                //
-                // eprintln!("- message: invalid secret references");
-                // eprintln!("- secret: {} \n", hint_str);
-                //
-                // let confirm = interaction::confirm_opt("Are you sure you want to continue?");
-                //
-                // if confirm.is_none() || (confirm.unwrap() == false) {
-                //     return Ok(());
-                // }
-            }
-
-            let mut spinner = request_spinner();
-            let res = secrets::set_sercrets(api_key, project, environment, &secrets).await;
-            debug!("{:#?}", res);
-
-            if let Err(err) = res {
-                spinner.stop_and_persist("", "");
-                debug!("Error: {:#?}", &err);
-                bail!(err);
-            }
-
-            let res = res.unwrap();
-
-            match res {
-                PostPatchRequestApiResponse::Ok(_) => {
-                    spinner.stop_with_message(&format!(
-                        "{} {}",
-                        "✓".green(),
-                        "Secrets have been uploaded!"
-                    ));
-                }
-                PostPatchRequestApiResponse::Err(e) => {
-                    debug!("Error: {}", e);
-                    spinner.stop_with_message(&format!("{}", e));
-                }
+    // let secrets_res = read_secrets_file(path);
+    //
+    let target_format = match format {
+        Some(format) => format,
+        None => {
+            if file_path.ends_with(".yaml") || file_path.ends_with(".yml") {
+                SecretsFileFormat::Yaml
+            } else if file_path.ends_with(".json") {
+                SecretsFileFormat::Json
+            } else {
+                SecretsFileFormat::Dotenv
             }
         }
-        Err(e) => {
-            bail!(format!("{} {}", "Error reading file:".red(), e));
+    };
+
+    let secrets_res = read_secrets_from_file(path, &target_format);
+
+    if let Err(err) = secrets_res {
+        let err = InputValidationError::Secrets(SecretsInputValidationError::ReadFile(err));
+        bail!(err);
+    }
+
+    let secrets = secrets_res.unwrap();
+
+    if secrets.is_empty() {
+        let msg = format!("{}: {}", "Nothing to upload".yellow(), "no secrets found");
+        eprintln!("{}", msg);
+
+        return Ok(());
+    }
+
+    let duplicate_keys = find_duplicate_keys(&secrets);
+
+    if !duplicate_keys.is_empty() {
+        let err = InputValidationError::Secrets(SecretsInputValidationError::DuplicateKeys(
+            duplicate_keys,
+        ));
+
+        bail!("{}", err);
+    }
+
+    let refs_validation = validate_secrets_references_with_existence(&secrets);
+
+    if !refs_validation.self_referenced_secrets.is_empty() {
+        let err = InputValidationError::Secrets(SecretsInputValidationError::SelfReferences(
+            refs_validation.self_referenced_secrets,
+        ));
+        bail!(err);
+    } else if !refs_validation.invalid_format.is_empty() || !refs_validation.not_found.is_empty() {
+        let mut print_str = String::new();
+
+        if !refs_validation.invalid_format.is_empty() {
+            let hint_str = refs_validation
+                .invalid_format
+                .iter()
+                .map(|(k, v)| format!("{} ({})", k, v.join(", ")))
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            print_str.push_str(&format!("- message: invalid secret references format\n"));
+            print_str.push_str(&format!("- secrets: {} \n", hint_str));
+        }
+
+        if !refs_validation.not_found.is_empty() {
+            let hint_str = refs_validation
+                .not_found
+                .iter()
+                .map(|(k, v)| format!("{} ({})", k, v.join(", ")))
+                .collect::<Vec<_>>()
+                .join(", ");
+
+            if !print_str.is_empty() {
+                print_str.push_str(&format!("\n"));
+            }
+
+            print_str.push_str(&format!(
+                "- message: referenced secrets not found within the file\n"
+            ));
+            print_str.push_str(&format!("- secret: {} \n", hint_str));
+        }
+
+        if !refs_validation.invalid_format.is_empty() && !refs_validation.not_found.is_empty() {
+            eprintln!("{}", format!("{}", "Input warnings").yellow());
+        } else {
+            eprintln!("{}", format!("{}", "Input warning").yellow());
+        }
+        eprintln!("{}", print_str);
+
+        let confirm = interaction::confirm_opt("Are you sure you want to continue?");
+
+        if confirm.is_none() || (confirm.unwrap() == false) {
+            return Ok(());
+        }
+
+        // let hint_str = references_validation
+        //     .invalid_format_references
+        //     .iter()
+        //     .map(|(k, v)| format!("{} ({})", k, v.join(", ")))
+        //     .collect::<Vec<_>>()
+        //     .join(", ");
+        //
+        // eprintln!("{}", format!("{}", "Input warning").yellow());
+        //
+        // eprintln!("- message: invalid secret references");
+        // eprintln!("- secret: {} \n", hint_str);
+        //
+        // let confirm = interaction::confirm_opt("Are you sure you want to continue?");
+        //
+        // if confirm.is_none() || (confirm.unwrap() == false) {
+        //     return Ok(());
+        // }
+    }
+
+    let mut spinner = request_spinner();
+    let res = secrets::set_sercrets(api_key, project, environment, &secrets).await;
+    debug!("{:#?}", res);
+
+    if let Err(err) = res {
+        spinner.stop_and_persist("", "");
+        debug!("Error: {:#?}", &err);
+        bail!(err);
+    }
+
+    let res = res.unwrap();
+
+    match res {
+        PostPatchRequestApiResponse::Ok(_) => {
+            spinner.stop_with_message(&format!(
+                "{} {}",
+                "✓".green(),
+                "Secrets have been uploaded!"
+            ));
+        }
+        PostPatchRequestApiResponse::Err(e) => {
+            debug!("Error: {}", e);
+            spinner.stop_with_message(&format!("{}", e));
         }
     }
 
