f78ddb2ef48b39760e908c0e1ef80be0b9b42c5c
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-07 08:58:00 +0200

    feat: change api path - rename secrets

--- src/api/secrets.rs
+++ src/api/secrets.rs
@@ -176,7 +176,7 @@ pub async fn rename_secrets(
         path: ApiPath::Secrets {
             project,
             environment,
-            path: Some(String::from("rename")),
+            path: None,
         },
         query: None,
         api_key,
