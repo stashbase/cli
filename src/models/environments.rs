099b1fcadf741f8e71784f7aed37813ad3cc4ea2
dimak <rahbussiness@gmail.com>
2024-08-16 11:31:05 +0200

    refactor: print environment item

--- src/models/environments.rs
+++ src/models/environments.rs
@@ -167,11 +167,12 @@ fn display_option(d: &Option<String>) -> String {
 
 impl Display for Environment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{} {}", "Env name:".green(), self.name)?;
-
         let (formatted, relative) = get_human_datetime(&self.created_at);
 
         writeln!(f, "{} {} ({})", "Created at:".green(), formatted, relative)?;
+
+        writeln!(f, "{} {}", "Id:".green(), self.id)?;
+        writeln!(f, "{} {}", "Name:".green(), self.name)?;
         writeln!(f, "{} {}", "Type:".green(), self.env_type)?;
         writeln!(f, "{} {}", "Locked:".green(), self.locked)?;
 
