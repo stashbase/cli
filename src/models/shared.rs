8d3ff27249b83c15b711026797df296bfea83a8c
dimak <rahbussiness@gmail.com>
2024-08-16 09:38:05 +0200

    feat: impl display for PaginationMetadata

--- src/models/shared.rs
+++ src/models/shared.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -15,4 +17,22 @@ pub struct PaginationMetadata {
     pub next_page: Option<usize>,
     // number of the previous page
     pub prev_page: Option<usize>,
+}
+
+// Implement the Display trait for PaginationMetadata
+impl fmt::Display for PaginationMetadata {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "------- Pagination Metadata -------")?;
+
+        write!(
+            f,
+            "Page: {} | Limit: {} | Next Page: {} | Prev Page: {} | Total Pages: {} |  Total Items: {} ",
+            self.page,
+            self.limit,
+            self.next_page.map_or("None".to_string(), |n| n.to_string()),
+            self.prev_page.map_or("None".to_string(), |p| p.to_string()),
+            self.total_items,
+            self.total_pages,
+        )
+    }
 }
