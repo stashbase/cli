f26e903bdfee5f67a57e8f0779503d7226b312f6
dimak <rahbussiness@gmail.com>
2024-08-17 12:54:14 +0200

    style: improve invalid identifier error hint

--- src/models/validation.rs
+++ src/models/validation.rs
@@ -228,12 +228,12 @@ impl fmt::Display for ProjectInputValidationError {
             }
             ProjectInputValidationError::InvalidIdentifierFormat { is_root } => {
                 if *is_root {
-                    let  hint_str = "The name or id must be alphanumeric, name may include underscores (_) and hyphens(-) and must be between 2 to 40 characters long. Id must start with the prefix 'pr_' and be exactly 25 characters long and consist of alphanumeric characters.";
+                    let  hint_str = "The name or id must be alphanumeric, name may include underscores (_) and hyphens(-) and must be between 2 to 40 characters long. Id must start with the prefix 'pr_' followed by 22 alphanumeric characters.";
 
                     msg = "argument name or id is invalid";
                     hint = Some(&hint_str);
                 } else {
-                    let  hint_str = "The project name or id must be alphanumeric, name may include underscores (_) and hyphens(-) and must be between 2 to 40 characters long. Id must start with the prefix 'pr_' and be exactly 25 characters long and consist of alphanumeric characters.";
+                    let  hint_str = "The project name or id must be alphanumeric, name may include underscores (_) and hyphens(-) and must be between 2 to 40 characters long. Id must start with the prefix 'pr_' followed by 22 alphanumeric characters.";
 
                     msg = "argument project is invalid";
                     hint = Some(&hint_str);
@@ -419,12 +419,12 @@ impl fmt::Display for EnvironmentsInputValidationError {
             }
             EnvironmentsInputValidationError::InvalidIdentifierFormat { is_root } => {
                 if *is_root {
-                    let  hint_str = "The name or id must be alphanumeric, name may include underscores (_) and a signle hyphen (-) as as separator and must be between 2 to 40 characters long. Id must start with the prefix 'ev_' and be exactly 25 characters long and consist of alphanumeric characters.";
+                    let  hint_str = "The name or id must be alphanumeric, name may include underscores (_) and a signle hyphen (-) as as separator and must be between 2 to 40 characters long. Id must start with the prefix 'ev_' followed by 22 alphanumeric characters.";
 
                     msg = "argument name or id is invalid";
                     hint = Some(&hint_str);
                 } else {
-                    let  hint_str = "The environment name or id must be alphanumeric, name may include underscores (_) and a signle hyphen (-) as as separator and must be between 2 to 40 characters long. Id must start with the prefix 'ev_' and be exactly 25 characters long and consist of alphanumeric characters.";
+                    let  hint_str = "The environment name or id must be alphanumeric, name may include underscores (_) and a signle hyphen (-) as as separator and must be between 2 to 40 characters long. Id must start with the prefix 'ev_' followed by 22 alphanumeric characters.";
 
                     msg = "argument environment is invalid";
                     hint = Some(&hint_str);
