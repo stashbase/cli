370fac61c18085b96c7bcbc77e066ba76eb29399
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-22 12:55:30 +0200

    feat: add new "ScanCommand"

---
+++ src/cmd/scan.rs
@@ -0,0 +1,24 @@
+use clap::Args;
+
+#[derive(Debug, Args)]
+#[command(override_usage = "scan [OPTIONS]")]
+pub struct ScanCommand {
+    /// Scan only selected files
+    #[clap(value_parser, long="files", num_args = 1..)]
+    pub files: Vec<String>,
+
+    /// Scan only git staged files
+    #[arg(value_enum, long = "print")]
+    pub staged: bool,
+
+    /// Autofix found issues
+    #[arg(value_enum, long = "autofix")]
+    pub autofix: bool,
+
+    /// Save the result in the cloud
+    #[arg(value_enum, long = "remote")]
+    pub remove: bool,
+    ///// Do not print any output
+    //#[arg(value_enum, long = "silent")]
+    //pub silent: bool,
+}
