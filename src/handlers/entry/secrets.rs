c955156dc56fa3a32f9dcec301680c31e49dec13
Radim Höfer <radim@stashbase.dev>
2024-11-01 16:44:45 +0100

    refactor: get search secrets output format

--- src/handlers/entry/secrets.rs
+++ src/handlers/entry/secrets.rs
@@ -40,12 +40,16 @@ pub async fn handle_secrets_commands(
     if let SecretSubcommand::Search(args) = cmd.subcommand {
         let format = match raw_output {
             true => SecretsSearchOutputFormat::Json,
-            false => match default_output_format {
-                Some(default_format) => {
-                    let search_format: Option<SecretsSearchOutputFormat> = default_format.into();
-                    search_format.unwrap_or_default()
-                }
-                None => SecretsSearchOutputFormat::default(),
+            false => match args.format {
+                Some(format) => format,
+                None => match default_output_format {
+                    Some(default_format) => {
+                        let search_format: Option<SecretsSearchOutputFormat> =
+                            default_format.into();
+                        search_format.unwrap_or_default()
+                    }
+                    None => SecretsSearchOutputFormat::default(),
+                },
             },
         };
 
