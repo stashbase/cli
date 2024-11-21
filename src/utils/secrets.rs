291ff313df798f799656e0a151d71e89e2aee94a
Radim Höfer <radim.engineering@stashbase.dev>
2024-11-21 11:34:38 +0100

    feat: use linked hash set for secret references extraction function

--- src/utils/secrets.rs
+++ src/utils/secrets.rs
@@ -1,11 +1,8 @@
 use anyhow::{bail, Context, Result};
+use linked_hash_set::LinkedHashSet;
 use log::debug;
 use regex::Regex;
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-    path::Path,
-};
+use std::{collections::HashMap, fs, path::Path};
 
 use colored_json::to_colored_json_auto;
 use owo_colors::OwoColorize;
@@ -789,16 +786,16 @@ pub fn find_duplicate_names(array: &[Secret]) -> Vec<String> {
         .collect()
 }
 
-pub fn extract_unique_references_from_secret(secret_value: &str) -> HashSet<String> {
+pub fn extract_unique_references_from_secret(secret_value: &str) -> LinkedHashSet<String> {
     // Define the regular expression to match ${...}
     let regex = Regex::new(r"\$\{(.*?)\}").unwrap();
     // Create a HashSet to store unique references
-    let mut refs = HashSet::new();
+    let mut refs = LinkedHashSet::new();
 
     // Iterate over all matches
     for cap in regex.captures_iter(secret_value) {
         // cap[1] contains the captured group inside ${}
-        refs.insert(cap[1].to_string());
+        refs.insert_if_absent(cap[1].to_string());
     }
 
     return refs;
