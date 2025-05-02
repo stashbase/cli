8cca802290e4df20ff9ba00ca13358542b72cb9b
Radim Höfer <radim@stashbase.dev>
2025-05-02 11:48:34 +0200

    refactor: standardize error message formatting in terminal size retrieval for consistency

--- src/utils/term_size.rs
+++ src/utils/term_size.rs
@@ -2,7 +2,7 @@ use terminal_size::{terminal_size, Height as TerminalHeight, Width as TerminalWi
 
 pub fn get_terminal_size() -> (usize, usize) {
     let (TerminalWidth(width), TerminalHeight(height)) =
-        terminal_size().expect("failed to obtain a terminal size");
+        terminal_size().expect("Failed to obtain a terminal size.");
 
     (width as usize, height as usize)
 }
