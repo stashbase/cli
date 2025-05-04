cccacc72cb72df51c76fd462adb3867bce6b2fd0
Radim Höfer <radim@stashbase.dev>
2025-05-04 16:35:36 +0200

    refactor: remove unnecessary spinner persistence in handle_get_secrets for cleaner output

--- src/handlers/secrets/get.rs
+++ src/handlers/secrets/get.rs
@@ -79,9 +79,7 @@ pub async fn handle_get_secrets(args: HandleGetSecretsArgs) -> anyhow::Result<()
                     if format == SecretsOutputFormat::Json {
                         let json_str = get_colored_json(&secrets).unwrap();
 
-                        spinner.stop_and_persist("", "");
                         println!("{}", json_str);
-
                         return Ok(());
                     }
 
