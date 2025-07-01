586c16f3f428959f4de5345292cade14be59a033
Radim Höfer <radim.engineering@stashbase.dev>
2025-07-01 08:17:57 +0200

    feat: add API request for scans

---
+++ src/api/scans.rs
@@ -0,0 +1,35 @@
+use super::client;
+use crate::models::{
+    api_client::{ApiPath, OutputError, RequestApiOptionResponse, RequestArgs},
+    scans::{PushCommitHunksPayload, StagedFileHunksPayload},
+};
+
+pub async fn scan_staged_hunks(
+    api_key: String,
+    data: &StagedFileHunksPayload,
+) -> Result<RequestApiOptionResponse, OutputError> {
+    let args = RequestArgs {
+        path: ApiPath::Scan {
+            path: "hunks".to_string(),
+        },
+        query: None,
+        api_key,
+    };
+
+    client::post_request(args, Some(data)).await
+}
+
+pub async fn scan_push_commit_hunks(
+    api_key: String,
+    data: &PushCommitHunksPayload,
+) -> Result<RequestApiOptionResponse, OutputError> {
+    let args = RequestArgs {
+        path: ApiPath::Scan {
+            path: "commits".to_string(),
+        },
+        query: None,
+        api_key,
+    };
+
+    client::post_request(args, Some(data)).await
+}
