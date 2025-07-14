3c699a89aac76831d149a2d0ae29acc856d3f976
Radim Höfer <radim@stashbase.dev>
2025-07-14 08:44:34 +0200

    feat: apply conditional terminal colorization to success and error messages in secret creation handler

--- src/handlers/secrets/create.rs
+++ src/handlers/secrets/create.rs
@@ -1,6 +1,5 @@
 use anyhow::{bail, Result};
 use log::debug;
-use owo_colors::OwoColorize;
 
 use crate::{
     api::secrets,
@@ -10,7 +9,10 @@ use crate::{
         validation::{InputValidationError, SecretsInputValidationError},
     },
     utils::{
-        interaction, output::get_colored_json, secrets::format_secret_comment, separator,
+        interaction,
+        output::{get_colored_json, ColorizeIfTerminal},
+        secrets::format_secret_comment,
+        separator,
         spinner::request_spinner,
     },
 };
@@ -194,9 +196,9 @@ pub async fn handle_create_secrets(args: HandleCreateSecretsArgs) -> Result<()> 
                                         "{} {}",
                                         format!(
                                             "{} {} {}",
-                                            "Secrets".green(),
-                                            "created".green(),
-                                            format!("({}):", created_count).green(),
+                                            "Secrets".green_if_tty_stderr(),
+                                            "created".green_if_tty_stderr(),
+                                            format!("({}):", created_count).green_if_tty_stderr(),
                                         ),
                                         secrets_created
                                             .iter()
@@ -237,9 +239,10 @@ pub async fn handle_create_secrets(args: HandleCreateSecretsArgs) -> Result<()> 
                                     "{} {}",
                                     format!(
                                         "{} {} {}",
-                                        "Secrets".red(),
-                                        "already exist".red(),
-                                        format!("({}):", duplicate_secrets.len()).red(),
+                                        "Secrets".red_if_tty_stderr(),
+                                        "already exist".red_if_tty_stderr(),
+                                        format!("({}):", duplicate_secrets.len())
+                                            .red_if_tty_stderr(),
                                     ),
                                     duplicate_secrets.join(", ")
                                 );
