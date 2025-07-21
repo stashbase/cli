8c8af3ee905897eac2703f430ed23566d0e3bec0
Radim Höfer <radim@stashbase.dev>
2025-07-21 12:19:56 +0200

    refactor: update handle_generate_random_value to use GenerateRandomValueAlphabet instead of GenerateRandomValueVariant

--- src/handlers/entry/generate.rs
+++ src/handlers/entry/generate.rs
@@ -4,6 +4,7 @@ use crate::{
         random::handle_generate_random_value,
         uuid::{handle_generate_uuid_v4, handle_generate_uuid_v7},
     },
+    models::generate::GenerateRandomValueAlphabet,
 };
 use anyhow::Result;
 
@@ -14,7 +15,13 @@ pub fn handle_generate_command(cmd: GenerateCommand, json_format: bool) -> Resul
             GenerateUuidSubcommand::V7 => handle_generate_uuid_v7(json_format),
         },
         GenerateSubcommand::Random(args) => {
-            handle_generate_random_value(args.subcommand, json_format, args.length, args.uppercase)
+            let alphabet = GenerateRandomValueAlphabet::from(args.subcommand);
+            handle_generate_random_value(
+                alphabet,
+                json_format,
+                args.get_length(),
+                args.get_uppercase(),
+            )
         }
     }
 
