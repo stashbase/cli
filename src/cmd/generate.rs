71ce2b8ac04db301bd9ff227d1f89061310c5648
Radim Höfer <radim.engineering@stashbase.dev>
2025-07-21 12:11:51 +0200

    feat: enhance GenerateRandom to support multiple subcommands and options for random string generation

--- src/cmd/generate.rs
+++ src/cmd/generate.rs
@@ -31,31 +31,43 @@ pub enum GenerateUuidSubcommand {
 pub struct GenerateRandom {
     #[clap(subcommand)]
     pub subcommand: GenerateRandomSubcommand,
+}
 
+#[derive(Debug, Subcommand)]
+pub enum GenerateRandomSubcommand {
+    Alphanumeric(GenerateRandomOptions),
+    Hex(GenerateRandomOptions),
+    Base64(GenerateRandomOptions),
+    Base64Url(GenerateRandomOptions),
+}
+
+impl GenerateRandom {
+    pub fn get_length(&self) -> usize {
+        match &self.subcommand {
+            GenerateRandomSubcommand::Alphanumeric(options) => options.length,
+            GenerateRandomSubcommand::Hex(options) => options.length,
+            GenerateRandomSubcommand::Base64(options) => options.length,
+            GenerateRandomSubcommand::Base64Url(options) => options.length,
+        }
+    }
+
+    pub fn get_uppercase(&self) -> bool {
+        match &self.subcommand {
+            GenerateRandomSubcommand::Alphanumeric(options) => options.uppercase,
+            GenerateRandomSubcommand::Hex(options) => options.uppercase,
+            GenerateRandomSubcommand::Base64(options) => options.uppercase,
+            GenerateRandomSubcommand::Base64Url(options) => options.uppercase,
+        }
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct GenerateRandomOptions {
     /// Length of the random string, defaults to 32
     #[arg(long = "length", default_value = "32")]
-    pub length: usize,
+    pub length: u32,
 
     /// Include uppercase letters
     #[arg(long = "uppercase")]
     pub uppercase: bool,
 }
-
-#[derive(Debug, Subcommand)]
-pub enum GenerateRandomSubcommand {
-    Alphanumeric,
-    Hex,
-    Base64,
-    Base64Url,
-}
-
-// #[derive(Debug, Args)]
-// pub struct GenerateRandomOptions {
-//     /// Length of the random string, defaults to 32
-//     #[arg(long = "length", default_value = "32")]
-//     pub length: u32,
-
-//     /// Include uppercase letters
-//     #[arg(long = "uppercase")]
-//     pub uppercase: bool,
-// }
