eec7991b6b23e05e0122fed9304881c4a2dc143b
Radim Höfer <radim.engineering@stashbase.dev>
2025-07-21 13:24:14 +0200

    refactor: rename GenerateRandomStringAlphabet to Encoding and update related implementations for consistency

--- src/models/generate.rs
+++ src/models/generate.rs
@@ -1,21 +1,19 @@
 use crate::cmd::generate::GenerateRandomStringSubcommand;
 
-pub enum GenerateRandomStringAlphabet {
+pub enum Encoding {
     Alphanumeric,
     Hex,
     Base64,
     Base64Url,
 }
 
-impl From<GenerateRandomStringSubcommand> for GenerateRandomStringAlphabet {
+impl From<GenerateRandomStringSubcommand> for Encoding {
     fn from(value: GenerateRandomStringSubcommand) -> Self {
         match value {
-            GenerateRandomStringSubcommand::Alphanumeric(_) => {
-                GenerateRandomStringAlphabet::Alphanumeric
-            }
-            GenerateRandomStringSubcommand::Hex(_) => GenerateRandomStringAlphabet::Hex,
-            GenerateRandomStringSubcommand::Base64(_) => GenerateRandomStringAlphabet::Base64,
-            GenerateRandomStringSubcommand::Base64Url(_) => GenerateRandomStringAlphabet::Base64Url,
+            GenerateRandomStringSubcommand::Hex(_) => Encoding::Hex,
+            GenerateRandomStringSubcommand::Base64(_) => Encoding::Base64,
+            GenerateRandomStringSubcommand::Base64Url(_) => Encoding::Base64Url,
+            GenerateRandomStringSubcommand::Alphanumeric(_) => Encoding::Alphanumeric,
         }
     }
 }
