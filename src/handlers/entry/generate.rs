23968f9ce607ca9b9249e92b66c67ec8d6a2c5b9
Radim Höfer <radim.engineering@stashbase.dev>
2025-07-21 12:26:21 +0200

    refactor: rename handle_generate_random_value to handle_generate_random_string for clarity and consistency

--- src/handlers/entry/generate.rs
+++ src/handlers/entry/generate.rs
@@ -1,7 +1,7 @@
 use crate::{
     cmd::generate::{GenerateCommand, GenerateSubcommand, GenerateUuidSubcommand},
     handlers::generate::{
-        random::handle_generate_random_value,
+        random::handle_generate_random_string,
         uuid::{handle_generate_uuid_v4, handle_generate_uuid_v7},
     },
     models::generate::GenerateRandomStringAlphabet,
@@ -19,7 +19,7 @@ pub fn handle_generate_command(cmd: GenerateCommand, json_format: bool) -> Resul
             let uppercase = args.get_uppercase();
             let alphabet = GenerateRandomStringAlphabet::from(args.subcommand);
 
-            handle_generate_random_value(alphabet, json_format, length, uppercase)
+            handle_generate_random_string(alphabet, json_format, length, uppercase)
         }
     }
 
