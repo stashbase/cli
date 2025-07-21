e85bf042fffcdc557d7af286df5b0cb090d4875b
dimak <rahbussiness@gmail.com>
2025-07-21 12:25:45 +0200

    docs: add docs for generate commands

--- src/cmd/generate.rs
+++ src/cmd/generate.rs
@@ -9,7 +9,10 @@ pub struct GenerateCommand {
 
 #[derive(Debug, Subcommand)]
 pub enum GenerateSubcommand {
+    /// Generate UUID
     Uuid(GenerateUuid),
+
+    /// Generate random string
     Random(GenerateRandomString),
 }
 
@@ -22,7 +25,10 @@ pub struct GenerateUuid {
 
 #[derive(Debug, Subcommand)]
 pub enum GenerateUuidSubcommand {
+    /// Generate UUID v4
     V4,
+
+    /// Generate UUID v7
     V7,
 }
 
@@ -35,9 +41,16 @@ pub struct GenerateRandomString {
 
 #[derive(Debug, Subcommand)]
 pub enum GenerateRandomStringSubcommand {
+    /// Generate random alphanumeric string
     Alphanumeric(GenerateRandomOptions),
+
+    /// Generate random hexadecimal string
     Hex(GenerateRandomOptions),
+
+    /// Generate random base64 string
     Base64(GenerateRandomOptions),
+
+    /// Generate random base64url string
     Base64Url(GenerateRandomOptions),
 }
 
@@ -67,7 +80,7 @@ pub struct GenerateRandomOptions {
     #[arg(long = "length", default_value = "32")]
     pub length: usize,
 
-    /// Include uppercase letters
+    /// Make the random string uppercase
     #[arg(long = "uppercase")]
     pub uppercase: bool,
 }
