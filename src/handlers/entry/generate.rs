2dbb11cee3866ba4dfc20eb931ab0d9d87f76848
dimak <rahbussiness@gmail.com>
2025-07-21 09:42:16 +0200

    feat: implement handle_generate_command to manage UUID and random value generation

---
+++ src/handlers/entry/generate.rs
@@ -0,0 +1,25 @@
+use crate::{
+    cmd::generate::{GenerateCommand, GenerateSubcommand, GenerateUuidSubcommand},
+    handlers::generate::{
+        random::handle_generate_random_value,
+        uuid::{handle_generate_uuid_v4, handle_generate_uuid_v7},
+    },
+};
+use anyhow::Result;
+
+pub fn handle_generate_command(cmd: GenerateCommand, json_format: bool) -> Result<()> {
+    match cmd.subcommand {
+        GenerateSubcommand::Uuid(args) => match args.subcommand {
+            GenerateUuidSubcommand::V4 => handle_generate_uuid_v4(json_format),
+            GenerateUuidSubcommand::V7 => handle_generate_uuid_v7(json_format),
+        },
+        GenerateSubcommand::Random(args) => handle_generate_random_value(
+            args.subcommand,
+            json_format,
+            args.length as usize,
+            args.uppercase,
+        ),
+    }
+
+    Ok(())
+}
