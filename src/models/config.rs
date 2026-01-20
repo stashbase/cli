1936414bae0288cc4bf4ebc9ff740317cba4d273
Radim Höfer <radim.engineering@stashbase.dev>
2026-01-20 10:32:06 +0100

    feat: add optional scope field to Config and UpdateConfig structs for enhanced configuration flexibility

--- src/models/config.rs
+++ src/models/config.rs
@@ -1,9 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-use crate::cmd::config::{OutputFormat, SecretsOutputFormat};
+use crate::cmd::{
+    config::{OutputFormat, SecretsOutputFormat},
+    shared::Scope,
+};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
+    pub scope: Option<Scope>,
     pub api_key: Option<String>,
     pub expand_refs: Option<bool>,
     pub ouput_format: Option<OutputFormatConfig>,
@@ -27,6 +31,7 @@ impl OutputFormatConfig {
 impl Config {
     pub fn new() -> Self {
         Self {
+            scope: None,
             api_key: None,
             ouput_format: None,
             expand_refs: None,
@@ -34,9 +39,15 @@ impl Config {
     }
     pub fn is_empty(&self) -> bool {
         if let Some(output_format) = &self.ouput_format {
-            self.api_key.is_none() && output_format.is_empty() && self.expand_refs.is_none()
+            self.api_key.is_none()
+                && self.scope.is_none()
+                && output_format.is_empty()
+                && self.expand_refs.is_none()
         } else {
-            self.api_key.is_none() && self.ouput_format.is_none() && self.expand_refs.is_none()
+            self.api_key.is_none()
+                && self.scope.is_none()
+                && self.ouput_format.is_none()
+                && self.expand_refs.is_none()
         }
     }
 }
@@ -49,6 +60,7 @@ impl OutputFormatConfig {
 
 #[derive(Debug)]
 pub struct UpdateConfig {
+    pub scope: Option<Scope>,
     pub api_key: Option<String>,
     pub expand_refs: Option<bool>,
     pub output_format: Option<OutputFormatConfig>,
