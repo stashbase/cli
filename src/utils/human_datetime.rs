8e0927b49f24d61c1d4b1777ffe9d254fa925f3f
Radim Höfer <radim.engineering@stashbase.dev>
2026-04-22 07:24:59 +0200

    chore: remove commented-out code from get_human_datetime function

--- src/utils/human_datetime.rs
+++ src/utils/human_datetime.rs
@@ -3,18 +3,6 @@ use chrono::Local;
 use chrono_humanize::HumanTime;
 use log::debug;
 
-// pub fn get_human_datetime(date_time_str: String) -> (String, HumanTime) {
-//
-//     let datetime = DateTime::<Local>::from(d);
-//
-//     let date_time = datetime.format("%Y-%m-%d %H:%M").to_string();
-//
-//     let dt = datetime - chrono::Local::now();
-//     let ht = chrono_humanize::HumanTime::from(dt);
-//
-//     return (date_time, ht);
-// }
-//
 pub fn get_human_datetime(date_time_str: &str) -> (String, HumanTime) {
     match DateTime::parse_from_rfc3339(date_time_str) {
         Ok(d) => {
