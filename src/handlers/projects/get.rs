116fc836cb74205f95a532a2369f507367907439
Radim Höfer <radim.engineering@stashbase.dev>
2026-04-22 07:36:30 +0200

    chore: remove commented-out code for listing projects

--- src/handlers/projects/get.rs
+++ src/handlers/projects/get.rs
@@ -110,48 +110,4 @@ pub async fn handle_get_project(
     }
 
     Ok(())
-    //
-    // let project_res = projects::list_projects(api_key).await;
-    // spinner.stop_and_persist("", "");
-    //
-    // if let Err(err) = &project_res {
-    //     error!("{:#?}", &err);
-    //     bail!("Could not connect to API")
-    // }
-    //
-    // let project_res = project_res.unwrap();
-    //
-    // let status = project_res.status();
-    //
-    // if status == 401 {
-    //     bail!("Unauthorized")
-    // }
-    //
-    // let response_text = project_res.text().await;
-    // debug!("{:#?}", &response_text);
-    //
-    // match response_text {
-    //     Ok(text) => {
-    //         let projects = serde_json::from_str::<Vec<Project>>(&text);
-    //
-    //         match projects {
-    //             Ok(projects) => {
-    //                 debug!("{:#?}", &projects);
-    //                 let value = serde_json::to_value(&projects).unwrap();
-    //                 let pretty = to_colored_json_auto(&value).unwrap();
-    //
-    //                 println!("{}", pretty);
-    //             }
-    //             Err(e) => {
-    //                 error!("{:#?}", &e);
-    //                 bail!("Something went wrong")
-    //             }
-    //         }
-    //     }
-    //     Err(err) => {
-    //         bail!("Could not parse response: {:?}", err);
-    //     }
-    // }
-    //
-    // Ok(())
 }
