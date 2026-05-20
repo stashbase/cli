bf0bec76671557a1687dd59ae5cda74423709b9a
Radim Höfer <radim.engineering@stashbase.dev>
2026-05-20 13:16:35 +0200

    refactor: add signing_secret field to Webhook struct

--- src/models/webhooks.rs
+++ src/models/webhooks.rs
@@ -45,6 +45,9 @@ pub struct Webhook {
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub signing_secret: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Tabled)]
