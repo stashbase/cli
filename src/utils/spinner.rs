6d750320385b20dab625fd5e3106510e02720f24
Radim Höfer <radim.engineering@stashbase.dev>
2026-06-18 12:27:46 +0200

    refactor: update spinner implementation for platform-specific behavior

--- src/utils/spinner.rs
+++ src/utils/spinner.rs
@@ -3,7 +3,15 @@ use std::borrow::Cow;
 use spinoff::{spinners, Color, Spinner, Streams};
 
 pub fn new_spinner(message: impl Into<Cow<'static, str>>, stream: Streams) -> Spinner {
-    Spinner::new_with_stream(spinners::Dots, message, Color::Cyan, stream)
+    #[cfg(windows)]
+    {
+        Spinner::new_with_stream(spinners::Line, message, Color::Cyan, stream)
+    }
+
+    #[cfg(not(windows))]
+    {
+        Spinner::new_with_stream(spinners::Dots, message, Color::Cyan, stream)
+    }
 }
 
 pub fn request_spinner() -> Spinner {
